3bce274e61b8dc6dba2116f985db191623c95135
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-02-15 13:00:33 +0530

    Create git_utility_metadata.sql
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/load/git_utility_metadata.sql
@@ -0,0 +1,15 @@
+USE DATABASE PROD_DNA_LOAD;
+USE SCHEMA META_RAW;
+
+create table if not exists META_RAW.GIT_RUN_METADATA (
+	PROCESS_ID NUMBER(38,0) autoincrement start 1 increment 1 order,
+	DB VARCHAR(50),
+	VERSION NUMBER(38,0),
+	FILE_NAME VARCHAR(50),
+	STATUS VARCHAR(50),
+	TIMESTAMP TIMESTAMP_NTZ(9)
+);
+
+INSERT INTO META_RAW.GIT_RUN_METADATA(db,version,file_name,status,timestamp) values
+('load',0,null,null,current_timestamp),
+('core',0,null,null,current_timestamp);
