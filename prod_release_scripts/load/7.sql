24d114c9a4e45697440229ce629600153e64217c
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-19 18:43:00 +0530

    Update 7.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/7.sql
+++ prod_release_scripts/load/7.sql
@@ -1,1 +1,8 @@
-update meta_raw.parameters set PARAMETER_NAME = 'transformationType' where PARAMETER_VALUE in ('ingestion','transformation');
+update meta_raw.usecase
+set USECASE_NAME='POP6_refresh' ,CATEGORY='POP6_refresh', USECASE_DESCRIPTION='Tableau Refresh Adhoc FOR POP6'
+where usecase_id=13;
+INSERT INTO meta_raw.USECASE (USECASE_ID, USECASE_NAME,CATEGORY,USECASE_DESCRIPTION,IS_ACTIVE,SEQUENCE_ID) VALUES (39,'CUSTOMER360_refresh','CUSTOMER360_refresh','Tableau Refresh Adhoc FOR CUSTOMER360','TRUE',1);
+
+update meta_raw.process
+set USECASE_ID=39
+where process_id=31;
