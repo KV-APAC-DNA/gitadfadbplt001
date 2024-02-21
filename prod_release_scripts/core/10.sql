6aa35994081fd9648bdc89e9b5287ec43a90c35a
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-21 13:16:00 +0530

    Update 10.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/core/10.sql
+++ prod_release_scripts/core/10.sql
@@ -1,4 +1,4 @@
-create or replace table  aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES (
+create  table if not exists  aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES (
     source_table_name varchar(200),
     source_view_name varchar(200),
     target_table_name varchar(200),
@@ -7,6 +7,7 @@ create or replace table  aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES (
     is_deleted boolean default 'False'
 );
 
+truncate table aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES;
 
 insert into aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES 
 with table_ as (
@@ -19,3 +20,34 @@ from aspitg_integration.vw_stg_sdl_sap_bw_sales
 group by act_file_name
 )
 select t.*,current_timestamp()::timestamp_ntz(9) as inserted_on,'False' as is_deleted from table_ t;
+
+
+insert into aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES 
+with table_ as (
+select 
+    'BWA_CDL_DELIVERY' as source_table_name,
+    'vw_stg_sdl_sap_bw_delivery' as source_view_name,
+    'itg_delivery_fact' as target_table_name,
+    filename as act_file_name
+from aspitg_integration.vw_stg_sdl_sap_bw_delivery
+group by act_file_name
+)
+select t.*,current_timestamp()::timestamp_ntz(9) as inserted_on,'False' as is_deleted from table_ t;
+
+
+
+
+--=============================================sdl_raw_sap_bw_delivery--===================================
+
+insert into aspwks_integration.SAP_TRANSACTIONAL_PROCESSED_FILES 
+with table_ as (
+select 
+    'BWA_CDL_DELIVERY' as source_table_name,
+    'vw_stg_sdl_sap_bw_delivery' as source_view_name,
+    'sdl_raw_sap_bw_delivery' as target_table_name,
+    filename as act_file_name
+from aspitg_integration.vw_stg_sdl_sap_bw_delivery
+group by act_file_name
+)
+select t.*,current_timestamp()::timestamp_ntz(9) as inserted_on,'False' as is_deleted from table_ t;
+
