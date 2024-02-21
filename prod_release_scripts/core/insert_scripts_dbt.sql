8a828d601cab2f9bb0055ee01218b775d09aa8e7
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-21 20:29:07 +0530

    Create insert_scripts_dbt.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/insert_scripts_dbt.sql
@@ -0,0 +1,106 @@
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_customer_sales','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_customer_sales__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_ecc_material_text','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_ecc_material_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_base_product_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_base_product_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_brand_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_brand_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_company_code_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_company_code_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_company','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_company__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_customer_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_customer_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_mega_brand_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_mega_brand_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_customer_base','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_customer_base__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_sales_org_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_sales_org_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_sales_office_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_sales_office_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_sales_org','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_sales_org__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_profit_center','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_profit_center__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_variant_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_variant_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_tcurr','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_tcurr__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_profit_center_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_profit_center_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_put_up_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_put_up_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_sales_group_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_sales_group_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_bw_strongholds_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_bw_strongholds_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_bw_time','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_bw_time__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_ecc_marm ','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_ecc_marm __duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_bw_country_code_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_bw_country_code_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_bw_needstates_text','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_bw_needstates_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_code_descriptions','select * from aspwks_integration.TRATBL_vw_stg_sdl_code_descriptions__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_dstrbtn_chnl','select * from aspwks_integration.TRATBL_vw_stg_sdl_dstrbtn_chnl__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_prod_hier','select * from aspwks_integration.TRATBL_vw_stg_sdl_prod_hier__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_material_typ','select * from aspwks_integration.TRATBL_vw_stg_sdl_material_typ__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_material_dim','select * from aspwks_integration.TRATBL_vw_stg_sdl_material_dim__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_cust_sls_attr','select * from aspwks_integration.TRATBL_vw_stg_sdl_cust_sls_attr__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_sap_bw_0account','select * from aspwks_integration.TRATBL_vw_stg_sdl_sap_bw_0account__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_ecc_account_text','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_ecc_account_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_ecc_material_plant','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_ecc_material_plant__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_ecc_material_sales','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_ecc_material_sales__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_bw_material_plant_text','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_bw_material_plant_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_bw_material_sales_text','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_bw_material_sales_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_bw_plant_attr','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_bw_plant_attr__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_bw_plant_text','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_bw_plant_text__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values(' vw_stg_sdl_sap_bw_dna_material_bomlist','select * from aspwks_integration.TRATBL_ vw_stg_sdl_sap_bw_dna_material_bomlist__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('vw_stg_sdl_account_attr_ciw','selecy * from aspwks_integration.TRATBL_vw_stg_sdl_account_attr_ciw__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_rg_ecom_plan','select * from aspwks_integration.TRATBL_sdl_mds_rg_ecom_plan__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_rg_ecom_plan__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_rg_profit_center_franchise_mapping','select * from aspwks_integration.TRATBL_sdl_mds_rg_profit_center_franchise_mapping__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_rg_profit_center_franchise_mapping__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_rg_ps_market_coverage','select * from aspwks_integration.TRATBL_sdl_mds_rg_ps_market_coverage__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_rg_ps_market_coverage__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_rg_ecom_digital_shelf_customer_mapping','select * from aspwks_integration.TRATBL_sdl_mds_rg_ecom_digital_shelf_customer_mapping__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_rg_ecom_digital_shelf_customer_mapping__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_ap_greenlight_skus','select * from aspwks_integration.TRATBL_sdl_mds_ap_greenlight_skus__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_ap_greenlight_skus__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_apac_dcl_products','select * from aspwks_integration.TRATBL_sdl_mds_apac_dcl_products__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_apac_dcl_products__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_apac_dcl_targets','select * from aspwks_integration.TRATBL_sdl_mds_apac_dcl_targets__null_test union all
+select * from aspwks_integration.TRATBL_sdl_mds_apac_dcl_targets__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_watsons','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_watsons__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_watsons__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_dfi','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_dfi__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_dfi__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_guardian','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_guardian__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_guardian__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_marketplace','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_marketplace__null_test
+union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_marketplace__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_scommerce','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_scommerce__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_scommerce__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_redmart','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_redmart__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_redmart__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_ntuc','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_ntuc__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_ntuc__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_scan_data_amazon','select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_amazon__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_scan_data_amazon__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_zuellig_customer_mapping','select * from sgpwks_integration.TRATBL_sdl_sg_zuellig_customer_mapping__null_test 
+union all
+select * from  sgpwks_integration.TRATBL_sdl_sg_zuellig_customer_mapping__duplicate_test
+union all
+select * from sgpwks_integration.TRATBL_sdl_sg_zuellig_customer_mapping__lookup_test
+');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_zuellig_sellout','select * from sgpwks_integration.TRATBL_sdl_sg_zuellig_sellout__lookup_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_zuellig_product_mapping','select * from sgpwks_integration.TRATBL_sdl_sg_zuellig_product_mapping__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_zuellig_product_mapping__lookup_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_tp_closed_month',' select * from sgpwks_integration.TRATBL_sdl_sg_tp_closed_month__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_tp_closed_month__lookup_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_sg_tp_closed_year_bal','select * from sgpwks_integration.TRATBL_sdl_sg_tp_closed_year_bal__null_test union all select * from sgpwks_integration.TRATBL_sdl_sg_tp_closed_year_bal__lookup_test');
+
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_rg_travel_retail_cdfg','select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_cdfg__null_test union all
+select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_cdfg_product__lookup_test union all
+select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_cdfg_channel__lookup_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_rg_travel_retail_cnsc','select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_cnsc__null_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_rg_travel_retail_dfs','select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_dfs__null_test  union all select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_dfs_product__lookup_test union all
+select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_dfs_channel__lookup_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_rg_travel_retail_dfs_hainan','select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_dfs_hainan__null_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_rg_travel_retail_dufry_hainan','select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_dufry_hainan__null_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_apac_dcl_customers','select * from  aspwks_integration.TRATBL_sdl_mds_apac_dcl_customers__null_test union all     select * from aspwks_integration.TRATBL_sdl_mds_apac_dcl_customers__duplicate_test');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_rg_travel_retail_sales_stock','select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_sales_stock__null_test union all select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_sales_stock_product__lookup_test union all select * from aspwks_integration.TRATBL_sdl_rg_travel_retail_sales_stock_channel__lookup_test');
+-----------------------------------------------------------------------------------------------------------
+
+
+insert into core_integration.dbtjobs_test_cdc_metadata values('J_RG_Account_attr_CIW_Dim','1','(\'vw_stg_sdl_account_attr_ciw\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('rg_mds_to_dna','2','(\'sdl_mds_rg_ecom_plan\',\'sdl_mds_rg_profit_center_franchise_mapping\',\'sdl_mds_rg_ps_market_coverage\',\'sdl_mds_rg_ecom_digital_shelf_customer_mapping\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('sap_bw_master_etl_framework','3','(\'vw_stg_sdl_sap_bw_0account\', \'vw_stg_sdl_sap_ecc_account_text\', \'vw_stg_sdl_sap_ecc_material_plant\', \'vw_stg_sdl_sap_ecc_material_sales\', \'vw_stg_sdl_sap_bw_material_plant_text\', \'vw_stg_sdl_sap_bw_material_sales_text\', \'vw_stg_sdl_sap_bw_plant_attr\', \'vw_stg_sdl_sap_bw_plant_text\', \'vw_stg_sdl_sap_bw_dna_material_bomlist\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('sap_ecc_master_etl_framework','4','(\'vw_stg_sdl_sap_ecc_customer_sales\', \'vw_stg_sdl_sap_ecc_material_text\',\'vw_stg_sdl_sap_ecc_base_product_text\',\'vw_stg_sdl_sap_ecc_brand_text\',\'vw_stg_sdl_sap_ecc_company_code_text\',\'vw_stg_sdl_sap_ecc_company\',\'vw_stg_sdl_sap_ecc_customer_text\',\'vw_stg_sdl_sap_ecc_mega_brand_text\',\'vw_stg_sdl_sap_ecc_customer_base\',\'vw_stg_sdl_sap_ecc_sales_org_text\',\'vw_stg_sdl_sap_ecc_sales_office_text\',\'vw_stg_sdl_sap_ecc_sales_org\',\'vw_stg_sdl_sap_ecc_profit_center\',\'vw_stg_sdl_sap_ecc_variant_text\',\'vw_stg_sdl_sap_ecc_tcurr\',\'vw_stg_sdl_sap_ecc_profit_center_text\',\'vw_stg_sdl_sap_ecc_put_up_text\',\'vw_stg_sdl_sap_ecc_sales_group_text\',\'vw_stg_sdl_sap_bw_strongholds_text\',\'vw_stg_sdl_sap_bw_time\',\'vw_stg_sdl_sap_ecc_marm \',\'vw_stg_sdl_sap_bw_country_code_text\',\'vw_stg_sdl_sap_bw_needstates_text\',\'vw_stg_sdl_code_descriptions\',\'vw_stg_sdl_dstrbtn_chnl\',\'vw_stg_sdl_prod_hier\',\'vw_stg_sdl_material_typ\',\'vw_stg_sdl_material_dim\',\'vw_stg_sdl_cust_sls_attr\',)','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('rg_greenlight_mds','5','(\'sdl_mds_ap_greenlight_skus\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('Global_Commercial_Hierarchy_Wrapper','6','(\'\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('rg_travel_retail_files','7','(\'sdl_mds_apac_dcl_customers\',
+\'sdl_mds_apac_dcl_products\',
+\'sdl_mds_apac_dcl_targets\'
+)','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('rg_travel_retail_files','8','(\'sdl_mds_apac_dcl_customers\',
+\'sdl_mds_apac_dcl_products\',
+\'sdl_mds_apac_dcl_targets\',\'sdl_mds_apac_dcl_products
+\',\'sdl_mds_apac_dcl_products\',\'sdl_rg_travel_retail_sales_stock\',\'sdl_rg_travel_retail_cdfg\', \'sdl_rg_travel_retail_cnsc\', \'sdl_rg_travel_retail_dfs\', \'sdl_rg_travel_retail_dfs_hainan\', \'sdl_rg_travel_retail_dufry_hainan\',\'sdl_mds_apac_dcl_customers\'
+)','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('sg_mds_to_dna_refresh','9','(\'\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('sg_master_sellout','10','(\'sdl_sg_zuellig_customer_mapping\',\'sdl_sg_zuellig_product_mapping\',\'itg_sg_ciw_mapping\',\'itg_sg_zuellig_sellout\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('sg_transaction_scan','11','(\'sdl_sg_scan_data_watsons\',\'sdl_sg_scan_data_dfi\',\'sdl_sg_scan_data_guardian\',\'sdl_sg_scan_data_marketplace\',\'sdl_sg_scan_data_scommerce\',\'sdl_sg_scan_data_redmart\',\'sdl_sg_scan_data_ntuc\',\'sdl_sg_scan_data_amazon\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('sg_transaction_sellout','12','(\'sdl_sg_tp_closed_month\',\'sdl_sg_tp_closed_year_bal\',\'sdl_sg_zuellig_product_mapping\')
+','0');
