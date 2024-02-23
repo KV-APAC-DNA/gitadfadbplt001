003792126e86783a81e7605d9bac482c6f113b1a
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-23 11:06:39 +0530

    Update COPA.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/core/COPA.sql
+++ prod_release_scripts/core/COPA.sql
@@ -1,5 +1,5 @@
 delete from aspitg_integration.itg_copa_trans
-where file_name='SAP_BW_COPA10_20240221_020315.csv'
+where file_name='SAP_BW_COPA10_20240221_020315.csv';
 --=============================
 DELETE FROM PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_INVC_SLS
 WHERE FILE_NAME IN(
