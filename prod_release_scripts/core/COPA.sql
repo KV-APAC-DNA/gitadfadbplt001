41d8e769cc5e27b8bf525c236a3a23c27c7c61e9
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-23 11:05:10 +0530

    Update and rename INVS.sql to COPA.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/COPA.sql
@@ -0,0 +1,7 @@
+delete from aspitg_integration.itg_copa_trans
+where file_name='SAP_BW_COPA10_20240221_020315.csv'
+--=============================
+DELETE FROM PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_INVC_SLS
+WHERE FILE_NAME IN(
+'SAP_BW_ZC_SD_20240221_011039.csv',
+'SAP_BW_ZC_SD_20240220_011101.csv');
