fa1c942399458ef7b54648217fa6679a642a4ebb
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-28 23:38:30 +0530

    Delete prod_release_scripts/load/9.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/9.sql
+++
@@ -1,7 +0,0 @@
-USE DATABASE PROD_DNA_LOAD;
--- Update PROD_DNA_LOAD.META_RAW.usecase set is_active= 'TRUE' where usecase_id in (3,37);
-
-Update PROD_DNA_LOAD.META_RAW.usecase set SEQUENCE_ID = 2  where usecase_id = 3;
-Update PROD_DNA_LOAD.META_RAW.usecase set SEQUENCE_ID = 3  where usecase_id = 37;
-
-
