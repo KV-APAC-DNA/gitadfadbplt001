a33f01c2fcf1394b32f46289b1554a0b21e633d2
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-29 18:20:21 +0530

    Update 4.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/4.sql
+++ prod_release_scripts/load/4.sql
@@ -2,6 +2,6 @@ update set  META_RAW.PARAMETERS
 PARAMETER_VALUE = 'prd/transaction/sellout/sales'
 where parameter_name = 'folder_path' and PARAMETER_GROUP_ID in (90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115);
 
-update s3_to_adls set
+update META_RAW.s3_to_adls set
 ADLS_PATH = = 'prd/transaction/sellout/sales'
 where s3_path = 'ap_my_metadata/transaction/sellout/sales'
