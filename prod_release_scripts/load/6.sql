e635afd1733d98b6cfde82bfb20bfab27cfd359c
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-02-29 18:25:21 +0530

    Update and rename 5.sql to 6.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/6.sql
+++ prod_release_scripts/load/6.sql
@@ -1,4 +1,4 @@
-update set  META_RAW.PARAMETERS 
+update META_RAW.PARAMETERS set
 PARAMETER_VALUE = 'prd/transaction/sellout/sales'
 where parameter_name = 'folder_path' and PARAMETER_GROUP_ID in (90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115);
 
