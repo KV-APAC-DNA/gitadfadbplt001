5d8d89e974efcfa4cca4c46da7e21659d0334d90
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-03-11 16:17:29 +0530

    Update and rename 48.sql to 49.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

---
+++ prod_release_scripts/load/49.sql
@@ -0,0 +1,3 @@
+update PROD_DNA_LOAD.META_RAW.PARAMETERS
+    set parameter_value= 'HTB,MEMBERS,HAIKOU BUGOU,Cambodia Downtown,BEIJING AIRPORT,XHG'
+    where parameter_group_name='rg_travel_retail_cdfg_group' and parameter_id =123
