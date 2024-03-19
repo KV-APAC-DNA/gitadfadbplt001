70c2a026b4ebe0ad284a1e21c6bb21374db55f35
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-03-19 11:50:24 +0530

    Update and rename 77.sql to 78.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/77.sql
+++
@@ -1,6 +0,0 @@
-update meta_raw.s3_to_adls
-set S3_FILE='Dufry'
-WHERE ID = 4;
-
-update META_RAW.HISTORICAL_OBJ_METADATA 
-set isactive = TRUE;
---
+++ prod_release_scripts/load/78.sql
@@ -0,0 +1,6 @@
+update meta_raw.s3_to_adls
+set S3_FILE='Dufry Hainan '
+WHERE ID = 4;
+
+
+
