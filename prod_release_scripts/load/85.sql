6983288aafa25eb909ec3182c98cf92966d06948
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-03-21 00:22:48 +0530

    Update 85.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

--- prod_release_scripts/load/85.sql
+++ prod_release_scripts/load/85.sql
@@ -1,1 +1,1 @@
-update DEV_DNA_LOAD.META_RAW.parameters set parameter_value='transactional'  where parameter_id=124
+update PROD_DNA_LOAD.META_RAW.parameters set parameter_value='transactional'  where parameter_id=124
