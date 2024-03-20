889fcab8d697ef37043fb574a2b639d118e46075
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-03-21 00:21:52 +0530

    Update and rename 84.sql to 85.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

--- prod_release_scripts/load/84.sql
+++
@@ -1,3 +0,0 @@
-update META_RAW.PARAMETERS 
-set PARAMETER_VALUE = 'Regional Customer Inventory Cover and Health DS_sf'
-where  PARAMETER_ID = 2821;
---
+++ prod_release_scripts/load/85.sql
@@ -0,0 +1,1 @@
+update DEV_DNA_LOAD.META_RAW.parameters set parameter_value='transactional'  where parameter_id=124
