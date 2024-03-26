9141bf68e8f4a81b3bacb90f453993bf30e703d1
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-03-26 19:38:03 +0530

    Create 127.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/127.sql
@@ -0,0 +1,564 @@
+CREATE OR REPLACE PROCEDURE THASDL_RAW.SDL_TH_MT_BIGC("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS '
+from snowflake.snowpark.functions import col, lit, current_timestamp, trim, upper
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DecimalType,DateType
+import pandas as pd
+from datetime import datetime
+import snowflake.snowpark as snowpark
+def main(session:snowpark.Session, Param):
+    try:
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        target_table    = Param[3]
+        df_schema = StructType([
+            StructField("REPORT_CODE", StringType()),
+            StructField("SUPPLIER", StringType()),
+            StructField("BUSINESS_FORMAT", StringType()),
+            StructField("COMPARE", StringType()),
+            StructField("STORE", StringType()),
+            StructField("TRANSACTION_DATE", StringType()),
+            StructField("LY_COMPARE_DATE", StringType()),
+            StructField("REPORT_DATE", StringType()),
+            StructField("DIVISION", StringType()),
+            StructField("DEPARTMENT", StringType()),
+            StructField("SUBDEPARTMENT", StringType()),
+            StructField("CLASS", StringType()),
+            StructField("SUBCLASS", StringType()),
+            StructField("BARCODE", StringType()),
+            StructField("ARTICLE", StringType()),
+            StructField("ARTICLE_NAME", StringType()),
+            StructField("BRAND", StringType()),
+            StructField("MODEL", StringType()),
+            StructField("SALE_AMT_TY_BAHT", StringType()),
+            StructField("SALE_AMT_LY_BAHT", StringType()),
+            StructField("SALE_AMT_VAR", StringType()),
+            StructField("SALE_QTY_TY", StringType()),
+            StructField("SALE_QTY_LY", StringType()),
+            StructField("SALE_QTY_VAR", StringType()),
+            StructField("STOCK_TY_BAHT", StringType()),
+            StructField("STOCK_LY_BAHT", StringType()),
+            StructField("STOCK_VAR", StringType()),
+            StructField("STOCK_QTY_TY", StringType()),
+            StructField("STOCK_QTY_LY", StringType()),
+            StructField("STOCK_QTY_VAR", StringType()),
+            StructField("DAY_ON_HAND_TY", StringType()),
+            StructField("DAY_ON_HAND_LY", StringType()),
+            StructField("DAY_ON_HAND_DIFF", StringType())
+        ])
+        
+        df = session.read\\
+        .schema(df_schema)\\
+        .option("skip_header",1)\\
+        .option("field_delimiter", ",")\\
+        .option("encoding", "UTF-8") \\
+        .option("REPLACE_INVALID_CHARACTERS", True) \\
+        .option("field_optionally_enclosed_by", "\\"") \\
+        .option("null_if", "") \\
+        .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+
+        df = df.with_column("CRT_DTTM", lit(datetime.now().strftime("%Y%m%d%H%M%S"))) \\
+               .with_column("FILE_NAME", lit(file_name))
+
+        
+        snowdf = df.select(
+            "REPORT_CODE",
+            "SUPPLIER",
+            "BUSINESS_FORMAT",
+            "COMPARE",
+            "STORE",
+            "TRANSACTION_DATE",
+            "LY_COMPARE_DATE",
+            "REPORT_DATE",
+            "DIVISION",
+            "DEPARTMENT",
+            "SUBDEPARTMENT",
+            "CLASS",
+            "SUBCLASS",
+            "BARCODE",
+            "ARTICLE",
+            "ARTICLE_NAME",
+            "BRAND",
+            "MODEL",
+            "SALE_AMT_TY_BAHT",
+            "SALE_AMT_LY_BAHT",
+            "SALE_AMT_VAR",
+            "SALE_QTY_TY",
+            "SALE_QTY_LY",
+            "SALE_QTY_VAR",
+            "STOCK_TY_BAHT",
+            "STOCK_LY_BAHT",
+            "STOCK_VAR",
+            "STOCK_QTY_TY",
+            "STOCK_QTY_LY",
+            "STOCK_QTY_VAR",
+            "DAY_ON_HAND_TY",
+            "DAY_ON_HAND_LY",
+            "DAY_ON_HAND_DIFF",
+            "FILE_NAME",
+            "CRT_DTTM"
+        )
+        
+        # Check if DataFrame is empty
+        if snowdf.count() == 0:
+            return "No Data in table"
+            
+        # Archive
+        file_name1=("_").join(file_name.split("_")[0:5])+"_"+datetime.now().strftime("%Y%m%d%H%M%S")
+        snowdf.write.copy_into_location("@"+Param[1]+"/"+Param[2]+"/success/"+file_name1, header=True, OVERWRITE=True)
+        
+        # Write operation
+        snowdf.write.mode("append").save_as_table(stage_name.split(".")[0]+"."+target_table)
+        
+        # Success message
+        return "Success"
+        
+    except KeyError as key_error:
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+        
+    except pd.errors.MergeError as merge_error:
+        error_message = f"DataFrame merging error: {str(merge_error)}"
+        return error_message
+    
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return error_message
+';
+
+CREATE OR REPLACE PROCEDURE THASDL_RAW.SDL_TH_MT_MAKRO("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python','xlrd')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS '
+
+from snowflake.snowpark.functions import col, lit, current_timestamp, trim, upper
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DecimalType,DateType
+import pandas as pd
+from datetime import datetime
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.files import SnowflakeFile
+
+def main(session:snowpark.Session, Param):
+
+    try:
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        target_table    = Param[3]
+
+        full_path = "@"+stage_name+"/"+temp_stage_path+"/"+file_name
+        with SnowflakeFile.open(full_path, "rb", require_scoped_url = False) as f:
+            excel_data = pd.read_excel(f, header=13)
+
+        excel_data.columns = [col.strip().upper().replace(" ", "_") for col in excel_data.columns]
+        columns_to_rename = {
+            "ITEM_DESCRIPTION": "ITEM_DESC",
+            "ON_ORDER_+_IN_TRANSIT_QTY": "ORDER_IN_TRANSIT_QTY",
+            "LAST_RECEIVED_DATE": "LAST_RECV_DT",
+            "LAST_SOLD_DATE": "LAST_SOLD_DT"
+        }
+        excel_data.rename(columns=columns_to_rename, inplace=True)
+    
+        df = session.create_dataframe(excel_data)
+
+        # Calculate FILE_NAME from file_name
+        # Assuming "Makro_202108_20211117200459.xls" is the file format,
+        # and we want to extract "202108" for the CRTD_DTM
+        file_date_part = file_name[6:12]  # This will extract "202108"
+        df = df.with_column("TRANSACTION_DATE", lit(datetime.strptime(file_date_part, "%Y%m"))) \\
+               .with_column("FILE_NAME", lit(file_name)) \\
+               .with_column("CRTD_DTM", lit(datetime.now().strftime("%Y%m%d%H%M%S")))
+
+
+        snowdf = df.select(
+            "TRANSACTION_DATE",
+            "SUPPLIER_NUMBER",
+            "LOCATION_NUMBER",
+            "LOCATION_NAME",
+            "CLASS_NUMBER",
+            "SUBCLASS_NUMBER",
+            "ITEM_NUMBER",
+            "BARCODE",
+            "ITEM_DESC",
+            "EOH_QTY",
+            "ORDER_IN_TRANSIT_QTY",
+            "PACK_TYPE",
+            "MAKRO_UNIT",
+            "AVG_NET_SALES_QTY",
+            "NET_SALES_QTY_YTD",
+            "LAST_RECV_DT",
+            "LAST_SOLD_DT",
+            "STOCK_COVER_DAYS",
+            "NET_SALES_QTY_MTD",
+            "DAY_1",
+            "DAY_2",
+            "DAY_3",
+            "DAY_4",
+            "DAY_5",
+            "DAY_6",
+            "DAY_7",
+            "DAY_8",
+            "DAY_9",
+            "DAY_10",
+            "DAY_11",
+            "DAY_12",
+            "DAY_13",
+            "DAY_14",
+            "DAY_15",
+            "DAY_16",
+            "DAY_17",
+            "DAY_18",
+            "DAY_19",
+            "DAY_20",
+            "DAY_21",
+            "DAY_22",
+            "DAY_23",
+            "DAY_24",
+            "DAY_25",
+            "DAY_26",
+            "DAY_27",
+            "DAY_28",
+            "DAY_29",
+            "DAY_30",
+            "DAY_31",
+            "FILE_NAME",
+            "CRTD_DTM"
+        )
+
+        # Check if DataFrame is empty
+        if snowdf.count() == 0:
+            return "No Data in table"
+        
+        # Archive
+        file_name1=("_").join(file_name.split("_")[0:5])+"_"+datetime.now().strftime("%Y%m%d%H%M%S")
+        snowdf.write.copy_into_location("@"+Param[1]+"/"+Param[2]+"/success/"+file_name1, header=True, OVERWRITE=True)
+        
+        # Write operation
+        snowdf.write.mode("append").save_as_table(stage_name.split(".")[0]+"."+target_table)
+        
+        # Success message
+        return "Success"
+
+    except KeyError as key_error:
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+
+    except pd.errors.MergeError as merge_error:
+        error_message = f"DataFrame merging error: {str(merge_error)}"
+        return error_message
+    
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return error_message
+';
+
+CREATE OR REPLACE PROCEDURE THASDL_RAW.SDL_TH_MT_WATSONS("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python','xlrd','openpyxl')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS '
+from snowflake.snowpark.functions import col, lit, current_timestamp, trim, upper
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DecimalType,DateType
+import pandas as pd
+from datetime import datetime
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.files import SnowflakeFile
+
+def main(session:snowpark.Session, Param):
+    try:
+        ###### EXCEPTION - CHANGING CSV TO XLSX ######
+        file_name       = Param[0].replace(".csv", ".xlsx")
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        target_table    = Param[3]
+
+        full_path = "@"+stage_name+"/"+temp_stage_path+"/"+file_name
+        with SnowflakeFile.open(full_path, "rb", require_scoped_url = False) as f:
+            excel_data = pd.read_excel(f, sheet_name=3)
+
+        excel_data.columns = [col.strip().upper().replace(" ", "_").replace("\\n", "_") for col in excel_data.columns]
+        
+        columns_to_rename = {
+            "NON/SLOW": "NON_SLOW",
+            "NON/SLOW2": "NON_SLOW2",
+            "DEACTI_VATE_DATE": "DEACTIVATE_DATE",
+            "PRO8": "PRO2",
+            "%DISC": "DISC",
+            "PRO82": "PRO22",
+            "PRO8_%DISC": "PRO2_PERT_DISC",
+            "GROUP": "GROUP_W",
+            "WINS": "WIN",
+            "POG": "POG_2",
+            "P_O_G": "POG"
+        }
+        excel_data.rename(columns=columns_to_rename, inplace=True)
+    
+        df = session.create_dataframe(excel_data)
+        
+        # Adding transformations for specific columns if needed
+        df = df.with_column("DATE", lit(datetime.now().strftime("%Y%m%d")))\\
+               .with_column("FILE_NAME", lit(file_name))
+        snowdf = df.select(
+            "DIV",
+            "DEPT",
+            "CLASS",
+            "SUBCLASS",
+            "ITEM",
+            "ITEM_DESC",
+            "NON_SLOW",
+            "NON_SLOW2",
+            "FINANCE_STATUS",
+            "CREATE_DATETIME",
+            "PRIM_SUPPLIER",
+            "OLD_SUPP_NO",
+            "SUPP_DESC",
+            "LEAD_TIME",
+            "UNIT_COST",
+            "UNIT_RETAIL_ZONE5",
+            "ITEM_STATUS",
+            "STATUS_WH",
+            "STATUS_WH_UPDATE_DATE",
+            "STATUS_STORE",
+            "STATUS_STORE_UPDATE_DATE",
+            "STATUS_XDOCK",
+            "STATUS_XDOCK_UPDATE_DATE",
+            "SOURCE_METHOD",
+            "SOURCE_WH",
+            "POG",
+            "PRODUCT_TYPE",
+            "LABEL_UDA",
+            "BRAND",
+            "ITEM_TYPE",
+            "RETURN_POLICY",
+            "RETURN_TYPE",
+            "WH_WAC",
+            "IN_TAX",
+            "TAX_RATE",
+            "STOCK_CAT",
+            "ORDER_FLAG",
+            "NEW_ITEM_13WEEK",
+            "DEACTIVATE_DATE",
+            "WH_ON_ORDER",
+            "FIRST_RCV",
+            "PROMO_MONTH",
+            "SALES_TW",
+            "NET_AMT",
+            "NET_COST",
+            "SALE_AVG_QTY_13WEEKS",
+            "SALE_AVG_AMT_13WEEKS",
+            "SALE_AVG_COST13WEEKS",
+            "NET_QTY_YTD",
+            "NET_AMT_YTD",
+            "NET_COST_YTD",
+            "TURN_WK",
+            "WH_SOH",
+            "STORE_TOTAL_STOCK",
+            "TOTAL_STOCK_QTY",
+            "WH_STOCK_AMT",
+            "STORE_TOTAL_STOCK_AMT",
+            "TOTAL_STOCK_XVAT",
+            "PRO2",
+            "DISC",
+            "PRO22",
+            "PRO2_PERT_DISC",
+            "FIRST_DATE_SMS",
+            "AGING_SMS",
+            "GROUP_W",
+            "WIN",
+            "POG_2",
+            "FILE_NAME",
+            "DATE"
+        )
+        
+        # Check if DataFrame is empty
+        if snowdf.count() == 0:
+            return "No Data in table"
+            
+        # Archive
+        file_name1=("_").join(file_name.split("_")[0:5])+"_"+datetime.now().strftime("%Y%m%d%H%M%S")
+        snowdf.write.copy_into_location("@"+Param[1]+"/"+Param[2]+"/success/"+file_name1, header=True, OVERWRITE=True)
+        
+        # Write operation
+        snowdf.write.mode("append").save_as_table(stage_name.split(".")[0]+"."+target_table)
+        
+        # Success message
+        return "Success"
+        
+    except KeyError as key_error:
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+        
+    except pd.errors.MergeError as merge_error:
+        error_message = f"DataFrame merging error: {str(merge_error)}"
+        return error_message
+    
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return error_message
+';
+
+CREATE OR REPLACE PROCEDURE THASDL_RAW.SDL_TH_TESCO_TRANSDATA("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS '
+
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DateType
+from snowflake.snowpark.functions import col,lit,date_format,current_timestamp,to_timestamp, xmlget, flatten, get, when, substring, row_number
+from snowflake.snowpark import Window
+import pandas as pd
+from datetime import datetime
+import pytz
+
+def get_xml_element(
+        column:str,
+        element:str,
+        datatype:str,
+        with_alias:bool = True
+):
+    new_element = (
+        get(
+            xmlget(
+                col(column),
+                lit(element),
+            ),
+            lit(''$'')
+        )
+        .cast("string")
+    )
+
+    new_element = when(new_element=='''', None).otherwise(new_element).cast(datatype)
+
+    # alias needs to be optional
+    return (
+        new_element.alias(element) if with_alias else new_element
+    )
+    
+
+
+
+def main(session:snowpark.Session, Param):
+
+    try:
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        target_table    = Param[3]
+
+        df = session.read\\
+        .option("STRIP_OUTER_ELEMENT", False) \\
+        .xml("@" + stage_name + "/" + temp_stage_path + "/" + file_name) \\
+        .select(
+            xmlget(col("$1"), lit("invrptth")).alias("invrptth"),
+            xmlget(xmlget(col("$1"), lit("invrptth")), lit("ir_header")).alias("ir_header"),
+            xmlget(xmlget(col("$1"), lit("invrptth")), lit("ir_items")).alias("ir_items"),
+
+            get_xml_element("ir_header", "creation_date", "string"),
+            get_xml_element("ir_header", "supplier_ID", "string"),
+            get_xml_element("ir_header", "supplier_name", "string"),
+            get_xml_element("ir_header", "warehouse", "string"),
+            get_xml_element("ir_header", "delivery_point_name", "string"),
+            get_xml_element("ir_header", "IR_date", "string")
+        ) \\
+        .select(
+            "creation_date",
+            "supplier_ID",
+            "supplier_name",
+            "warehouse",
+            "delivery_point_name",
+            "IR_date",
+            flatten(col("ir_items"), "$")
+        ) \\
+        .select (
+            "creation_date",
+            "supplier_ID",
+            "supplier_name",
+            "warehouse",
+            "delivery_point_name",
+            "IR_date",
+            get_xml_element("value", "EANSKU", "string"),
+            get_xml_element("value", "article_ID", "string"),
+            get_xml_element("value", "SPN", "string"),
+            get_xml_element("value", "article_name", "string"),
+            get_xml_element("value", "stock", "string"),
+            get_xml_element("value", "sales", "string"),
+            get_xml_element("value", "sales_amount", "string"),
+        )
+
+        # Adding transformations for specific columns if needed
+        df = df.with_column("FOLDER_NAME", lit(temp_stage_path))\\
+               .with_column("FILE_NAME", lit(file_name))
+
+        # Remove entries with null IR_DATE
+        df_filtered = df.filter((col("IR_DATE").is_not_null()) & (col("IR_DATE").cast(StringType()) != ''''))
+
+        # Continue from the window specification and ranking
+        windowSpec = Window.partitionBy("IR_DATE", "WAREHOUSE", "SUPPLIER_ID").orderBy(substring(col("FILE_NAME"), 8, 26).desc())
+        df_with_rank = df_filtered.withColumn("RANK", row_number().over(windowSpec))
+
+        # Create a DataFrame of distinct file names where rank = 1
+        df_rank_1_files = df_with_rank.filter(col("RANK") == 1).select("FILE_NAME").distinct()
+
+        # Use a join operation to filter df_filtered based on the file names that have rank = 1
+        df_filtered = df_filtered.join(df_rank_1_files, "FILE_NAME", "inner")
+
+        snowdf = df_filtered.select(
+            "CREATION_DATE",
+            "SUPPLIER_ID",
+            "SUPPLIER_NAME",
+            "WAREHOUSE",
+            "DELIVERY_POINT_NAME",
+            "IR_DATE",
+            "EANSKU",
+            "ARTICLE_ID",
+            "SPN",
+            "ARTICLE_NAME",
+            "STOCK",
+            "SALES",
+            "SALES_AMOUNT",
+            "FILE_NAME",
+            "FOLDER_NAME"
+        )
+
+        # Check if DataFrame is empty
+        if snowdf.count() == 0:
+            return "No Data in table"
+
+        # Archive
+        file_name1=("_").join(file_name.split("_")[0:5])+"_"+datetime.now().strftime("%Y%m%d%H%M%S")
+        snowdf.write.copy_into_location("@"+Param[1]+"/"+Param[2]+"/success/"+file_name1, header=True, OVERWRITE=True)
+        
+        # Write operation
+        snowdf.write.mode("append").save_as_table(stage_name.split(".")[0]+"."+target_table)
+        
+        # Success message
+        return "Success"
+    except KeyError as key_error:
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+
+    except pd.errors.MergeError as merge_error:
+        error_message = f"DataFrame merging error: {str(merge_error)}"
+        return error_message
+    
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return error_message
+';
