d0fb849e94d4e6444c65ff13e55aebfc0ecaad39
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-03-28 16:40:51 +0530

    Update and rename 147.sql to 148.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/147.sql
+++
@@ -1,3 +0,0 @@
-update meta_raw.process
-set parameter_value = 'SFMC/master/TH_crm_consumer_master/TH_CRM_Consumer_Master_Additional'
-where parameter_id = 2972;
---
+++ prod_release_scripts/load/148.sql
@@ -0,0 +1,3 @@
+update meta_raw.PARAMETERS
+set PARAMETER_VALUE = 'SFMC/master/TH_crm_consumer_master/TH_CRM_Consumer_Master_Additional'
+where parameter_id = 2972;
