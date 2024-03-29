802168f3b74f7af252533b77b3f43294fa87e360
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-03-29 12:10:35 +0530

    Create 151.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/151.sql
@@ -0,0 +1,116 @@
+delete from meta_raw.s3_to_adls
+where id = 112;
+
+CREATE OR REPLACE PROCEDURE THASDL_RAW.TH_GT_ROUTE_HDR_PREPROCESSING("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+EXECUTE AS CALLER
+AS '
+
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DateType
+from snowflake.snowpark.functions import col,lit,date_format,current_timestamp,to_timestamp, md5, coalesce, concat, upper, trim, to_date
+import pandas as pd
+from datetime import datetime
+import pytz
+
+
+
+def main(session: snowpark.Session, Param): 
+    
+    try:
+        # Extracting parameters from the input
+
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        sch_name        = stage_name.split(''.'')[0]
+        target_table    = sch_name+"."+Param[3]
+        
+        # Set the current session schema
+        
+        session.use_schema(sch_name)
+
+        # Define the schema for the DataFrame
+        df_schema=StructType([
+            StructField("id", StringType(50)),
+            StructField("name", StringType(100)),
+            StructField("desc", StringType(100)),
+            StructField("is_active", StringType(10)),
+            StructField("routesale", StringType(50)),
+            StructField("saleunit", StringType(50)),
+            StructField("route_code", StringType(50)),
+            StructField("description", StringType(100)),
+            StructField("Last_Updated_date", StringType(20))
+            ])
+        # Set the current session schema
+        
+        # Read the CSV file into a DataFrame
+        df = session.read\\
+        .schema(df_schema)\\
+        .option("skip_header",1)\\
+        .option("field_delimiter", "|")\\
+        .option("field_optionally_enclosed_by", "\\"") \\
+        .option("truncatecolumns",True) \\
+        .option("skip_blank_lines", True) \\
+        .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+
+        uploadeddate = file_name.split("_")[-1].split(".")[0]
+        
+        #convertin time stamp into sg timezone
+        df = df.withColumn("CRT_DTTM", lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S")))
+        run_id=datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y%m%d%H%M%S")
+        new_file_name=file_name.split(".")[0]+"_"+run_id+".txt"
+        df = df.withColumn("FILE_NAME", lit(new_file_name).cast("string"))
+        df = df.withColumn("RUN_ID", lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y%m%d%H%M%S")))
+        df = df.withColumn("CNTRY_CD", lit("TH"))
+        df = df.withColumn("CRNCY_CD", lit("THB"))
+        df = df.withColumn("HASH_KEY", 
+            md5(concat( \\
+                    coalesce(upper(trim(col("saleunit"))), lit(''N/A'')) , \\
+                    coalesce(upper(trim(col("routesale"))), lit(''N/A'')) , \\
+                    coalesce(upper(trim(col("id"))), lit(''N/A'')) \\
+                ) \\
+            ) \\
+        )
+ 
+        df = df.withColumn("FILE_UPLOADED_DATE", to_timestamp(lit(uploadeddate),"YYYYMMDDHHMISS"))
+            
+        
+        # Creating copy of the Dataframe
+        final_df = df.select("HASH_KEY", "CNTRY_CD", "CRNCY_CD", "id", "name", "desc", \\
+            "is_active", "routesale", "saleunit", "route_code", "description", to_date("Last_Updated_date", lit("YYYYMMDD")).as_("Last_Updated_date"), \\
+            "FILE_NAME", "FILE_UPLOADED_DATE", "RUN_ID", "CRT_DTTM" ).alias("final_df")
+
+        if final_df.count()==0:
+            return "No Data in file"
+        
+        # Load Data to the target table
+        final_df.write.mode("append").saveAsTable(target_table)
+
+    
+        # write to success folder
+		
+        current_date = datetime.now()
+        formatted_year = current_date.strftime("%Y")
+        formatted_month = current_date.strftime("%m")
+    
+        file_name=file_name.split(".")[0]+''_''+datetime.now().strftime("%Y%m%d%H%M%S")
+		
+        final_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/"+"processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,header=True,OVERWRITE=True)
+   
+        return "Success"
+
+    except KeyError as key_error:
+        # Handle KeyError (missing columns) here
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+        
+    except Exception as e:
+        # Handle exceptions here
+        error_message = f"Error: {str(e)}"
+        return error_message
+';
