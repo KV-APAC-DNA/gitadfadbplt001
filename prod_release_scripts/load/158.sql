0d1c69e678fcf25294a5fa7239d1d9af22d11fda
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-01 15:18:17 +0530

    Create 158.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/158.sql
@@ -0,0 +1,7 @@
+UPDATE S3_TO_ADLS
+SET ADLS_PATH = 'GT_Intervention/master/DnA_VMR/MSL'
+WHERE ID = 90;
+
+UPDATE S3_TO_ADLS
+SET ADLS_PATH = 'GT_Intervention/master/DnA_VMR/RouteDtl'
+WHERE ID = 91;
