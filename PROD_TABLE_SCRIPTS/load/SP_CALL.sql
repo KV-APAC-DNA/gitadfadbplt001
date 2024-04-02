eb1d5f199457a9b7e6a6f95fd6f8c7c9df939848
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-04-02 23:08:18 +0530

    Create SP_CALL.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

---
+++ PROD_TABLE_SCRIPTS/load/SP_CALL.sql
@@ -0,0 +1,1 @@
+CALL PROD_DNA_LOAD.ASPSDL_RAW.FILE_VALIDATION(['Product.csv','full','','Product','csv','prodkey,Prodid,Proddesc,Ean,Active',0,'PCFSDL_RAW.DEV_LOAD_STAGE_ADLS','dev/ap_perenso/master/product/',''])
