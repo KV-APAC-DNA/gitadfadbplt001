a359bfa22a51620a517569c5860e1c9243b06057
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-04-02 22:23:11 +0530

    Veitnam metadata fixes
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/load/167.sql
@@ -0,0 +1,10 @@
+USE SCHEMA META_RAW;
+
+update parameters set parameter_value='dms/transaction/dms_source/' where parameter_name='folder_path' and parameter_group_id=220;
+update parameters set parameter_value='dms/transaction/dms_source/' where parameter_name='folder_path' and parameter_group_id=221;
+update parameters set parameter_value='dms/transaction/dms_source/' where parameter_name='folder_path' and parameter_group_id=222;
+update parameters set parameter_value='dms/transaction/dms_source/' where parameter_name='folder_path' and parameter_group_id=219;
+update parameters set parameter_value='dms/transaction/dms_source/' where parameter_name='folder_path' and parameter_group_id=223;
+update parameters set parameter_value='VN_PS/transaction/Interface_Answers/' where parameter_name='folder_path' and parameter_group_id=230;
+update parameters set parameter_value='VN_PS/transaction/Interface_Questions/' where parameter_name='folder_path' and parameter_group_id=237;
+update parameters set parameter_value='dksh/transaction/dksh_stock/' where parameter_name='folder_path' and parameter_group_id=297;
