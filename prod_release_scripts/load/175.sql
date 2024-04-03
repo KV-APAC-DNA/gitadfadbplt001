37ed198922d7b715bb4c23bfe07b394e86a0529f
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-03 16:19:32 +0530

    Create 175.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/175.sql
@@ -0,0 +1,5 @@
+INSERT INTO META_RAW.PROCESS VALUES (178,178,'vn_dms_tableaue_refresh',178,1,1,FALSE,TRUE,77,null,3,null,'','','','','','Tableau');
+
+update META_RAW.PROCESS
+SET USECASE_ID = 96
+WHERE PROCESS_ID IN (294,296);
