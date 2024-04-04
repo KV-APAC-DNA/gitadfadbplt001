1d02a523964d14d218b98e5460887e20eb39827d
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-04 15:38:29 +0530

    Create 180.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/180.sql
@@ -0,0 +1,142 @@
+CREATE OR REPLACE PROCEDURE THASDL_RAW.SDL_TH_MT_MAKRO("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python','xlrd')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS '
+
+from snowflake.snowpark.functions import col, lit, current_timestamp, trim, upper
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DecimalType,DateType
+import pandas as pd
+from datetime import datetime
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.files import SnowflakeFile
+
+def main(session:snowpark.Session, Param):
+
+    try:
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        target_table    = Param[3]
+
+        full_path = "@"+stage_name+"/"+temp_stage_path+"/"+file_name
+        with SnowflakeFile.open(full_path, "rb", require_scoped_url = False) as f:
+            excel_data = pd.read_excel(f, header=13)
+
+        excel_data.columns = [col.strip().upper().replace(" ", "_") for col in excel_data.columns]
+        columns_to_rename = {
+            "ITEM_DESCRIPTION": "ITEM_DESC",
+            "ON_ORDER_+_IN_TRANSIT_QTY": "ORDER_IN_TRANSIT_QTY",
+            "LAST_RECEIVED_DATE": "LAST_RECV_DT",
+            "LAST_SOLD_DATE": "LAST_SOLD_DT"
+        }
+        excel_data.rename(columns=columns_to_rename, inplace=True)
+    
+        df = session.create_dataframe(excel_data)
+
+        # Calculate FILE_NAME from file_name
+        # Assuming "Makro_202108_20211117200459.xls" is the file format,
+        # and we want to extract "202108" for the CRTD_DTM
+        file_date_part = file_name[6:12]  # This will extract "202108"
+        run_id=datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y%m%d%H%M%S")
+        new_file_name=file_name.split(".")[0]+''_''+run_id+''.txt''
+        df = df.with_column("TRANSACTION_DATE", lit(file_date_part)) \\
+               .with_column("FILE_NAME", lit(new_file_name)) \\
+               .with_column("CRTD_DTM", lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S")))
+
+        snowdf = df.select(
+            "TRANSACTION_DATE",
+            "SUPPLIER_NUMBER",
+            "LOCATION_NUMBER",
+            "LOCATION_NAME",
+            "CLASS_NUMBER",
+            "SUBCLASS_NUMBER",
+            "ITEM_NUMBER",
+            "BARCODE",
+            "ITEM_DESC",
+            "EOH_QTY",
+            "ORDER_IN_TRANSIT_QTY",
+            "PACK_TYPE",
+            "MAKRO_UNIT",
+            "AVG_NET_SALES_QTY",
+            "NET_SALES_QTY_YTD",
+            "LAST_RECV_DT",
+            "LAST_SOLD_DT",
+            "STOCK_COVER_DAYS",
+            "NET_SALES_QTY_MTD",
+            "DAY_1",
+            "DAY_2",
+            "DAY_3",
+            "DAY_4",
+            "DAY_5",
+            "DAY_6",
+            "DAY_7",
+            "DAY_8",
+            "DAY_9",
+            "DAY_10",
+            "DAY_11",
+            "DAY_12",
+            "DAY_13",
+            "DAY_14",
+            "DAY_15",
+            "DAY_16",
+            "DAY_17",
+            "DAY_18",
+            "DAY_19",
+            "DAY_20",
+            "DAY_21",
+            "DAY_22",
+            "DAY_23",
+            "DAY_24",
+            "DAY_25",
+            "DAY_26",
+            "DAY_27",
+            "DAY_28",
+            "DAY_29",
+            "DAY_30",
+            "DAY_31",
+            "FILE_NAME",
+            "CRTD_DTM"
+        )
+        
+        # Check if DataFrame is empty
+        snowdf = snowdf.na.drop("all")
+        if snowdf.count() == 0:
+            return "No Data in table"
+
+        # Archive to success
+        current_date = datetime.now()
+        formatted_year = current_date.strftime("%Y")
+        formatted_month = current_date.strftime("%m")
+
+        snowdf.write.copy_into_location(
+            "@"+stage_name+
+            "/"+temp_stage_path+
+            "/"+"processed/success/"+formatted_year+
+            "/"+formatted_month+
+            "/"+file_name,
+            header=True,
+            OVERWRITE=True
+        )
+        
+        # Write operation
+        snowdf.write.mode("append").save_as_table(stage_name.split(".")[0]+"."+target_table)
+        
+        # Success message
+        return "Success"
+
+    except KeyError as key_error:
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+
+    except pd.errors.MergeError as merge_error:
+        error_message = f"DataFrame merging error: {str(merge_error)}"
+        return error_message
+    
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return error_message
+';
