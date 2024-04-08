10c6b49233704c382b23834e4775a082aa565c15
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-04-08 13:18:35 +0530

    Create 195.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

---
+++ prod_release_scripts/load/195.sql
@@ -0,0 +1,7 @@
+update prod_dna_load.meta_raw.parameters
+set parameter_group_id='392'
+where parameter_id='4909';
+
+update prod_dna_load.meta_raw.parameters
+set parameter_group_id='393'
+where parameter_id='4915';
