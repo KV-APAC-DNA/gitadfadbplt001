6ab2941932589f916c5fa6eb436ef707f5a924a7
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-10 11:53:50 +0530

    Create 71.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/71.sql
@@ -0,0 +1,9 @@
+update prod_dna_core.vnmitg_integration.itg_vn_dms_distributor_dim
+set TERRITORY_DIST = 'TD001-Dương Anh',
+DSTRBTR_NAME = 'Chi nhánh Dương Anh tại Bình Dương',
+DSTRBTR_ADDRESS = 'Số 2/1H, đường ĐT 743B, Khu phố Bình Đức 3, Phường Bình Hòa, TP.Thuận An, Tỉnh Bình Dương  CHI NHANH DUONG ANH TẠI BINH DUONG',
+PROVINCE = 'Bình Dương',
+ASM_ID = 'VuHl'
+where TERRITORY_DIST = 'TD001-DÆ°Æ¡ng Anh'
+and DSTRBTR_ID = 'DABDUONG'
+;
