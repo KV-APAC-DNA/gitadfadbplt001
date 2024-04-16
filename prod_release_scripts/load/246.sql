0b095a1e863aa11cd4fe754dcc39c7dedb8188d2
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-04-16 11:08:42 +0530

    Create 246.sql
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/load/246.sql
@@ -0,0 +1,2 @@
+USE SCHEMA META_RAW;
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (178,15,'VN','itx-arm-conapdna-vietnam-prod-kdp','raw-data-lake/VN_MTI','SPIRAL_MT_OFFTAKE','vnm','VN_MTI/transaction/VN_MTI_OFFTAKE',TRUE,'Y');
