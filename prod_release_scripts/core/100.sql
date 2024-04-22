cb2ec6ac3e968bf72cd04c4258d26cbc1f798c34
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-22 17:11:48 +0530

    Update and rename 99.sql to 100.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/100.sql
@@ -0,0 +1,17 @@
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_call','50','(\'sdl_perenso_work_item\', \'sdl_perenso_diary_item_type\', \'sdl_perenso_call_objectives\', \'sdl_perenso_diary_item_time\') ','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_ranging','51','(\'sdl_perenso_ranging\', \'sdl_perenso_ranging_product\', \'sdl_perenso_ranging_acct_grp_rel\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_head_office_req','52','(\'sdl_perenso_todo\', \'sdl_perenso_head_office_req_state\', \'sdl_perenso_head_office_req_check\', \'sdl_perenso_store_chk_hdr\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_survey_ques','53','(\'sdl_perenso_todo_option\', \'sdl_perenso_survey_result\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_over_and_above','54','(\'sdl_perenso_over_and_above\', \'sdl_perenso_over_and_above_state\', \'sdl_perenso_prod_chk_distribution\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_account','55','(\'sdl_perenso_account_group_lvl\', \'sdl_perenso_account_group\', \'sdl_perenso_account_fields\', \'sdl_perenso_account_reln_id\', \'sdl_perenso_account_type\', \'sdl_perenso_account\', \'sdl_perenso_acct_dist_acct\', \'sdl_perenso_distributor_detail\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_foodstuff','56','(\'sdl_perenso_fsni_sales\', \'sdl_perenso_fssi_sales\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_product','57','(\'sdl_perenso_product_fields\', \'sdl_perenso_product_group\', \'sdl_perenso_product_group_lvl\', \'sdl_perenso_product_group_reln\', \'sdl_perenso_product_reln_id\', \'sdl_perenso_product\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_sales_rep','58','(\'sdl_perenso_diary_item\', \'sdl_perenso_users\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_perenso_order','59','(\'sdl_perenso_order_batch\', \'sdl_perenso_order_detail\', \'sdl_perenso_order_header\', \'sdl_perenso_order_type\', \'sdl_perenso_deal_discount\', \'sdl_perenso_constants\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_pharm_ecomm_load','60','(\'sdl_chw_ecomm_data\', \'sdl_national_ecomm_data\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('j_pa_pharma_inv','61','(\'sdl_api_dstr\', \'sdl_symbion_dstr\', \'sdl_sigma_dstr\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('j_pa_grocery_inv','62','(\'sdl_mds_pacific_product_mapping_coles\', \'sdl_mds_pacific_product_mapping_ww\') ','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('j_pac_iri_scan_sales_etl_framework','63','(\'sdl_iri_scan_sales\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('j_pacific_cogs_etl_framework','64','(\'sdl_mds_pacific_cogs_master\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('anz_mds_sdl_to_edw_load','65','(\'sdl_mds_pacific_cust_attrb\', \'sdl_mds_pacific_sapbw_plan_lkp\', \'sdl_mds_pacific_sap_gl_ciw_mapping\', \'sdl_mds_pacific_px_gl_ciw_mapping\', \'sdl_mds_pacific_perenso_cycle_dates\', \'sdl_mds_pacific_cust_ciw_plan\')','0');
+insert into core_integration.dbtjobs_test_cdc_metadata values('j_pac_pharm_data_load','66','(\'sdl_pharm_sellout_probe\', \'sdl_pharm_sellout_outlet\', \'sdl_pharm_sellout_product\')','0');
