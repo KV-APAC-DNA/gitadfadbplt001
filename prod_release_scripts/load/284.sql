953552a7f86f8a0492ef1e603fa33b90684805d6
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-23 23:12:33 +0530

    Update 284.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/284.sql
+++ prod_release_scripts/load/284.sql
@@ -1,3 +1,24 @@
+delete from meta_raw.parameters
+where parameter_id in (3551,3671,4202,4598,5967);
+
+delete from meta_raw.s3_to_adls
+where id in (135,182,191,196,207);
+
+
+INSERT INTO meta_raw.PARAMETERS (PARAMETER_ID, PARAMETER_GROUP_ID,PARAMETER_GROUP_NAME,PARAMETER_NAME,PARAMETER_VALUE,IS_SENSITIVE,IS_ACTIVE) VALUES (3551,280,'anz_perenso_overandabove_group','folder_path','ap_perenso/transaction/overandabove/OverandAbove',FALSE,TRUE);
+INSERT INTO meta_raw.PARAMETERS (PARAMETER_ID, PARAMETER_GROUP_ID,PARAMETER_GROUP_NAME,PARAMETER_NAME,PARAMETER_VALUE,IS_SENSITIVE,IS_ACTIVE) VALUES (3671,287,'anz_perenso_Ranging','folder_path','ap_perenso/transaction/ranging/Ranging',FALSE,TRUE);
+INSERT INTO meta_raw.PARAMETERS (PARAMETER_ID, PARAMETER_GROUP_ID,PARAMETER_GROUP_NAME,PARAMETER_NAME,PARAMETER_VALUE,IS_SENSITIVE,IS_ACTIVE) VALUES (4202,317,'perenso_product_group','folder_path','ap_perenso/master/product/ProdGrp',FALSE,TRUE);
+INSERT INTO meta_raw.PARAMETERS (PARAMETER_ID, PARAMETER_GROUP_ID,PARAMETER_GROUP_NAME,PARAMETER_NAME,PARAMETER_VALUE,IS_SENSITIVE,IS_ACTIVE) VALUES (4598,364,'AcctGrp','folder_path','ap_perenso/master/account/AcctGrp',FALSE,TRUE);
+INSERT INTO meta_raw.PARAMETERS (PARAMETER_ID, PARAMETER_GROUP_ID,PARAMETER_GROUP_NAME,PARAMETER_NAME,PARAMETER_VALUE,IS_SENSITIVE,IS_ACTIVE) VALUES (5967,451,'Account','folder_path','ap_perenso/master/account/Account',FALSE,TRUE);
+
+INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (135,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/transaction_files/overandabove/','OverandAbove','pac','ap_perenso/transaction/overandabove/OverandAbove',TRUE,'Y');
+INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (182,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/master/product/ProdGrp','ProdGrp','pac','ap_perenso/master/product/ProdGrp',TRUE,'Y');
+INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (191,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/master_files/account','AcctGrp','pac','ap_perenso/master/account/AcctGrp',TRUE,'Y');
+INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (196,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/master_files/account','Account','pac','ap_perenso/master/account/Account',TRUE,'Y');
+INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (207,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/transaction_files/ranging/','Ranging','pac','ap_perenso/transaction/ranging/Ranging',TRUE,'Y');
+
+
+
 CREATE OR REPLACE PROCEDURE PCF_PERENSO_PRODUCT_PREPROCESSING("PARAM" ARRAY)
 RETURNS VARCHAR(16777216)
 LANGUAGE PYTHON
