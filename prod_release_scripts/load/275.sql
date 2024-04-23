c58a9b87ec553e9a1ca30d2b806ca0e24b588dfe
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-23 14:39:46 +0530

    Update 275.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/275.sql
+++ prod_release_scripts/load/275.sql
@@ -16,3 +16,12 @@ where PARAMETER_GROUP_ID in (402,403,405,406);
 update meta_raw.parameters
 set sequence_id = 'AcctGrp'
 where PARAMETER_ID in (4592,4593);
+
+update meta_raw.parameters
+set sequence_id = 'Account'
+where PARAMETER_ID in (5961,5962);
+
+
+update meta_raw.parameters
+set sequence_id = 'Ranging'
+where PARAMETER_ID in (3678,3680);
