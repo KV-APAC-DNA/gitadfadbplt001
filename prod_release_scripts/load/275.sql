92fb225aaee6d794f6f3f008793c4e2f9a2b9844
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-23 14:33:09 +0530

    Update 275.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/275.sql
+++ prod_release_scripts/load/275.sql
@@ -8,3 +8,11 @@ create or replace TABLE PROD_DNA_LOAD.PCFSDL_RAW.SDL_PERENSO_SURVEY_RESULT (
 	RUN_ID NUMBER(14,0),
 	CREATE_DT TIMESTAMP_NTZ(9) DEFAULT CONVERT_TIMEZONE('SGT', CAST(CAST('CURRENT_TIMESTAMP()' AS TIMESTAMP_NTZ(9)) AS TIMESTAMP_TZ(9)))
 );
+
+update meta_raw.process
+set sequence_id = 1
+where PARAMETER_GROUP_ID in (402,403,405,406);
+
+update meta_raw.parameters
+set sequence_id = 'AcctGrp'
+where PARAMETER_ID in (4592,4593);
