9173dbe72f88f7f1fcca52172933c85814532cae
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-25 01:56:43 +0530

    Update 297.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/297.sql
+++ prod_release_scripts/load/297.sql
@@ -3,3 +3,5 @@ where id in(214,240);
 
 INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (214,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/transaction_files/Survey/','SurveyResult','pac','ap_perenso/transaction/survey',TRUE,'Y');
 INSERT INTO meta_raw.s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (240,17,'PCF','itx-arm-conapdna-pacific-prod-kdp','ap_perenso/transaction_files/Survey/','TodoOption','pac','ap_perenso/transaction/survey',TRUE,'Y');
+
+INSERT INTO PROCESS VALUES (361,361,'AcctGrpLevel',361,1,1,FALSE,TRUE,91,1,1,null,'','','','PCFSDL_RAW.PROD_LOAD_STAGE_ADLS','','');
