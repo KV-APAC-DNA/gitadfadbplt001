5a31c3313a81830c3aa76d9200eee1134201527b
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-04-25 13:47:39 +0530

    Update and rename 302.sql to 303.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/302.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO meta_raw.PROCESS VALUES (379,379,'j_pacific_cbg_data_ingestion_pdfs',379,1,1,FALSE,TRUE,135,null,3,null,'','','','','','Tableau');
-
-INSERT INTO PROCESS VALUES (362,362,'j_pac_iri_scan_sales_etl_framework',361,1,1,FALSE,TRUE,104,null,3,null,'','','','','','Tableau');
---
+++ prod_release_scripts/load/303.sql
@@ -0,0 +1,1 @@
+INSERT INTO meta_raw.PROCESS VALUES (362,362,'j_pac_iri_scan_sales_etl_framework',361,1,1,FALSE,TRUE,104,null,3,null,'','','','','','Tableau');
