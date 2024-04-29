68278bbccaffdef2b233ad1f011cc75a2a6d9c7b
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-04-30 00:38:30 +0530

    Update check_stage.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

--- PROD_TABLE_SCRIPTS/load/check_stage.sql
+++ PROD_TABLE_SCRIPTS/load/check_stage.sql
@@ -1,3 +1,3 @@
-create stage ASPSDL_RAW.PROD_LOAD_STAGE_ADLS
+create stage PHLSDL_RAW.PROD_LOAD_STAGE_ADLS
 storage_integration = PROD_DNA_LOAD_AZURE32_SI
 url = 'azure://dlsadbplt001.blob.core.windows.net/phl/';
