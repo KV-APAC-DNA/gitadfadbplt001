2f937214bfcd2e088c2640f3ec7428c3f7d1f238
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-05-03 11:21:32 +0530

    Update and rename MetaD1.sql to MetaData.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- PROD_TABLE_SCRIPTS/load/MetaD1.sql
+++
@@ -1,3 +0,0 @@
-update meta_raw.process
-set usecase_id = 130
-where usecase_id = 113;
---
+++ PROD_TABLE_SCRIPTS/load/MetaData.sql
@@ -0,0 +1,3 @@
+update meta_raw.parameters
+set PARAMETER_VALUE = 'IRI_Scan_Data/transaction/All_JJ_Items_WE'
+where PARAMETER_ID = 4305 and  parameter_group_id = 323;
