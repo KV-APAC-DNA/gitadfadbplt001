cc8bcdb87489fbe3a30e4656a92f6444f0322e54
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-05-03 16:15:26 +0530

    Update and rename MetaData1.sql to MetaData.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- PROD_TABLE_SCRIPTS/load/MetaData.sql
+++ PROD_TABLE_SCRIPTS/load/MetaData.sql
@@ -1,7 +1,7 @@
-update meta_raw.parmeters
+update meta_raw.parameters
 set PARAMETER_VALUE = 'xlsx'
 where PARAMETER_ID in (5702,5692);
-update meta_raw.parmeters
+update meta_raw.parameters
 set PARAMETER_VALUE = 'A7'
 where PARAMETER_ID = 5704;
 
