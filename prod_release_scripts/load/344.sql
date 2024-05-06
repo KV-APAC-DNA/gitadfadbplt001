ed925228ab2f8d93f1dff118cdbfedc67a8fc4a5
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-05-06 17:25:45 +0530

    Update 344.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

--- prod_release_scripts/load/344.sql
+++ prod_release_scripts/load/344.sql
@@ -1,3 +1,8 @@
 UPDATE META_RAW.PARAMETERS
 SET PARAMETER_VALUE = 'KENVUE_RetailPROBE_WE_202405'
 WHERE PARAMETER_ID = 7073 AND PARAMETER_NAME ='file_spec';
+
+update prod_dna_load.meta_raw.usecase
+set SEQUENCE_ID=3 where category='REGIONAL REFRESH' and usecase_name='PCF_CIW_EDW_Sales' and usecase_id=141;
+update prod_dna_load.meta_raw.usecase
+set SEQUENCE_ID=2 where category='REGIONAL REFRESH' and usecase_name='PCF_Promax_CIW' and usecase_id=142;
