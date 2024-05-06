85dc4c2791107ea8ee10d911285366a1830e28c7
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-05-06 17:25:18 +0530

    Create 345.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/345.sql
@@ -0,0 +1,4 @@
+update prod_dna_load.meta_raw.usecase
+set SEQUENCE_ID=3 where category='REGIONAL REFRESH' and usecase_name='PCF_CIW_EDW_Sales' and usecase_id=141;
+update prod_dna_load.meta_raw.usecase
+set SEQUENCE_ID=2 where category='REGIONAL REFRESH' and usecase_name='PCF_Promax_CIW' and usecase_id=142;
