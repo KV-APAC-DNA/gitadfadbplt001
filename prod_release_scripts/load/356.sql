ccfdb01e3d9727a6e2f972488214e02901e78326
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-05-07 19:01:35 +0530

    Create 356.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/356.sql
@@ -0,0 +1,5 @@
+use schema meta_raw;
+
+INSERT INTO PROCESS VALUES (366,366,'PH_POS_PG_SM_TDE_Refresh',366,1,1,FALSE,TRUE,177,null,3,null,'','','','','','Tableau');
+INSERT INTO PROCESS VALUES (367,367,'PH_GT_Scorecard_Tableau_Refresh',367,1,1,FALSE,TRUE,177,null,3,null,'','','','','','Tableau');
+INSERT INTO PROCESS VALUES (501,501,'PH_NPI_Refresh',501,1,1,FALSE,TRUE,176,null,3,null,'','','','','','Tableau');
