9b30315ecd3adc82fd820af1509bf36939a31060
rrajen01 <168544870+rrajen01@users.noreply.github.com>
2024-05-10 14:21:27 +0100

    Added IN Batch Master
    Signed-off-by: rrajen01 <168544870+rrajen01@users.noreply.github.com>

--- procedure_ddl/India_Procedure.sql
+++ procedure_ddl/India_Procedure.sql
@@ -597,3 +597,81 @@ def main(session: snowpark.Session,Param):
         error_message = f"Error: {str(e)}" +str(df.columns)
         return error_message
 $$;
+
+CREATE OR REPLACE PROCEDURE DEV_DNA_LOAD.INDSDL_RAW.IN_BATCH_MASTER_PREPROCESSING("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS '
+from snowflake.snowpark.functions import col, lit, date_format, current_timestamp, to_date, year, month, concat, format_number, regexp_replace,to_timestamp,when,trim,upper
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DecimalType,DateType
+from datetime import datetime
+import snowflake.snowpark as snowpark
+import pytz
+
+
+def main(session: snowpark.Session,Param):
+    try:
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        sch_name        = stage_name.split(''.'')[0]
+        target_table    = sch_name+"."+Param[3]
+
+        df_schema = StructType([
+                    StructField("statecode", StringType()),
+                    StructField("prodcode", StringType()),
+                    StructField("mrp", StringType()),
+                    StructField("lsp", StringType()),
+                    StructField("sellrate", StringType()),
+                    StructField("purchrate", StringType()),
+                    StructField("claimrate", StringType()),
+                    StructField("netrate", StringType()),
+                    StructField("createddt", StringType())
+                ])
+
+        df = session.read\\
+            .schema(df_schema)\\
+            .option("skip_header",1)\\
+            .option("field_delimiter", "|")\\
+            .option("field_optionally_enclosed_by", "\\"") \\
+            .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+
+        df = df.withColumn("FILE_NAME",lit(file_name).cast("string"))
+        df = df.withColumn("RUN_ID",lit(datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y%m%d%H%M%S")))
+        df = df.withColumn("CRT_DTTM",lit(datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")))
+
+        final_df = df.select("statecode", "prodcode", "mrp", "lsp", "sellrate", "purchrate","claimrate","netrate","createddt", \\
+             "FILE_NAME","RUN_ID","CRT_DTTM" ).alias("final_df")
+
+        if final_df.count()==0:
+            return "No Data in file"
+        
+        # Load Data to the target table
+        final_df.write.mode("append").saveAsTable(target_table)
+
+        current_date = datetime.now()
+        formatted_year = current_date.strftime("%Y")
+        formatted_month = current_date.strftime("%m")
+
+        # write to success folder
+    
+        file_name=file_name.split(".")[0] + ''_'' + datetime.now().strftime("%Y%m%d%H%M%S")
+        final_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,file_format_type="csv",OVERWRITE=True,header=True)
+   
+
+        return "Success"
+        
+    except KeyError as key_error:
+        
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+        
+    except Exception as e:
+        
+        error_message = f"Error: {str(e)}" +str(df.columns)
+        return error_message
+';
