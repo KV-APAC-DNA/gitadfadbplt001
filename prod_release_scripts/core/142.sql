de017a15f5f8847d32963f6ecd6bcf1261eb4c3b
AAgraw03 <148230421+AAgraw03@users.noreply.github.com>
2024-05-16 13:49:22 +0530

    Update and rename 142.sql to 143.sql
    Signed-off-by: AAgraw03 <148230421+AAgraw03@users.noreply.github.com>

--- prod_release_scripts/core/142.sql
+++
@@ -1,2 +0,0 @@
-delete from pcfedw_integration.edw_sls_evolution_history
-where snapshot_date='2024-05-16';
---
+++ prod_release_scripts/core/143.sql
@@ -0,0 +1,2 @@
+delete from pcfedw_integration.edw_sls_evolution
+where snapshot_date='2024-05-16';
