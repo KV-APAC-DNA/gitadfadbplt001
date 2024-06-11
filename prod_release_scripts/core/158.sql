a1410f34ae7f7bc321f79ffe2ad38dafbfa487b9
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-06-11 15:47:31 +0530

    Create 158.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/158.sql
@@ -0,0 +1,196 @@
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ref_mapping_channel','select * from idnwks_integration.TRATBL_sdl_mds_id_ref_mapping_channel__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ref_mapping_channel__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_customer_hierarchy','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_customer_hierarchy__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_customer_hierarchy__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ref_distributor_group','select * from idnwks_integration.TRATBL_sdl_mds_id_ref_distributor_group__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ref_distributor_group__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ref_mapping_product','select * from idnwks_integration.TRATBL_sdl_mds_id_ref_mapping_product__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ref_mapping_product__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ref_province','select * from idnwks_integration.TRATBL_sdl_mds_id_ref_province__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ref_province__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_distributor_group_ingest_include','select * from idnwks_integration.TRATBL_sdl_mds_id_distributor_group_ingest_include__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_distributor_group_ingest_include__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ref_salesman','select * from idnwks_integration.TRATBL_sdl_mds_id_ref_salesman__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ref_salesman__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_distributor_customer_update','select * from idnwks_integration.TRATBL_sdl_mds_id_distributor_customer_update__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_distributor_customer_update__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_dms_dist_margin_control','select * from idnwks_integration.TRATBL_sdl_mds_id_dms_dist_margin_control__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_dms_dist_margin_control__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_distributor_ivy_order','select * from idnwks_integration.TRATBL_sdl_distributor_ivy_order__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_distributor_ivy_order__test_lookup__ff;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_distributor_ivy_invoice','select * from idnwks_integration.TRATBL_sdl_distributor_ivy_invoice__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_distributor_ivy_invoice__test_lookup__ff;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_distributor_ivy_merchandising','select * from idnwks_integration.TRATBL_sdl_distributor_ivy_merchandising__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_distributor_ivy_merchandising__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_distributor_ivy_outlet_master','select * from idnwks_integration.TRATBL_sdl_distributor_ivy_outlet_master__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_distributor_ivy_outlet_master__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_pricing','select * from idnwks_integration.TRATBL_sdl_id_ps_pricing__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_pricing__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_promotion','select * from idnwks_integration.TRATBL_sdl_id_ps_promotion__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_promotion__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_promotion_competitor','select * from idnwks_integration.TRATBL_sdl_id_ps_promotion_competitor__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_promotion_competitor__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_5ps_store_mapping','select * from idnwks_integration.TRATBL_sdl_mds_id_5ps_store_mapping__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_5ps_store_mapping__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ps_priority_store','select * from idnwks_integration.TRATBL_sdl_mds_id_ps_priority_store__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ps_priority_store__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_planogram','select * from idnwks_integration.TRATBL_sdl_id_ps_planogram__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_planogram__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_secondary_display','select * from idnwks_integration.TRATBL_sdl_id_ps_secondary_display__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_secondary_display__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ps_brand','select * from idnwks_integration.TRATBL_sdl_mds_id_ps_brand__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ps_brand__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ps_weights','select * from idnwks_integration.TRATBL_sdl_mds_id_ps_weights__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ps_weights__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ps_targets','select * from idnwks_integration.TRATBL_sdl_mds_id_ps_targets__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_ps_msl','select * from idnwks_integration.TRATBL_sdl_mds_id_ps_msl__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_ps_msl__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_product_availability','select * from idnwks_integration.TRATBL_sdl_id_ps_product_availability__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_product_availability__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_ps_visibility','select * from idnwks_integration.TRATBL_sdl_id_ps_visibility__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_ps_visibility__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_raw_id_ps_brand_blocking','select * from idnwks_integration.TRATBL_sdl_raw_id_ps_brand_blocking__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_raw_id_ps_brand_blocking__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_stock_dist_map','select * from idnwks_integration.TRATBL_sdl_stock_dist_map__test_lookup__ff;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_dist_reporting_control','select * from idnwks_integration.TRATBL_sdl_mds_id_dist_reporting_control__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_dist_reporting_control__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_dist_reporting_control__lookup_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_dist_reporting_control__lookup_test2;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_sellin_target','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sellin_target__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sellin_target__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_product_hierarchy','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_product_hierarchy__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_product_hierarchy__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_bp_sop_target','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_bp_sop_target__lookup_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_bp_sop_target__lookup_test2
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_bp_sop_target__lookup_test3;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_sales_target','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sales_target__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sales_target__lookup_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sales_target__lookup_test2
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sales_target__lookup_test3
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_sales_target__lookup_test4;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_superindo_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_superindo_sellout__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_superindo_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_superindo_sellout__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_superindo_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_superindo_stock__date_format_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_superindo_stock__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_superindo_stock__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_carrefour_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_carrefour_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_carrefour_sellout__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_carrefour_sellout__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_carrefour_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_carrefour_stock__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_carrefour_stock__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_carrefour_stock__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_watson_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_watson_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_watson_sellout__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_watson_sellout__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_watson_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_watson_stock__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_watson_stock__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_watson_stock__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_sat_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_sat_stock__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_sat_stock__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_sat_stock__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_sat_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_sat_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_sat_sellout__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_sat_sellout__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_midi_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_midi_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_midi_sellout__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_midi_sellout__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_midi_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_midi_stock__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_midi_stock__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_midi_stock__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_diamond_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_diamond_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_diamond_sellout__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_diamond_sellout__date_format_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_daily_idm_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_daily_idm_sellout__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_hero_list','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_hero_list__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_hero_list__lookup_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_hero_list__lookup_test2
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_hero_list__lookup_test3;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_npi_sku_list','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_npi_sku_list__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_pos_cust_prod_mapping','select * from idnwks_integration.TRATBL_sdl_mds_id_pos_cust_prod_mapping__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_pos_cust_prod_mapping__duplicate_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_mds_id_lav_inventory_intransit','select * from idnwks_integration.TRATBL_sdl_mds_id_lav_inventory_intransit__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_inventory_intransit__duplicate_test
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_inventory_intransit__test_lookup
+union all
+select * from idnwks_integration.TRATBL_sdl_mds_id_lav_inventory_intransit__test_lookup2;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_guardian_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_guardian_stock__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_guardian_stock__date_check;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_idm_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_idm_sellout__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_idm_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_idm_stock__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_igr_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_igr_stock__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_igr_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_igr_sellout__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_matahari_otc_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_matahari_otc_stock__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_matahari_jb_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_matahari_jb_stock__null_test;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_matahari_beauty_stock','select * from idnwks_integration.TRATBL_sdl_id_pos_matahari_beauty_stock__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_matahari_beauty_stock__date_check;');
+insert into CORE_INTEGRATION.DBTTEST_TABLES_METADATA values('sdl_id_pos_daily_basedline_sellout','select * from idnwks_integration.TRATBL_sdl_id_pos_daily_basedline_sellout__null_test
+union all
+select * from idnwks_integration.TRATBL_sdl_id_pos_daily_basedline_sellout__duplicate_test;');
