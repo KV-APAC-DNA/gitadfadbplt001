872633d2e54b6055265fbf1b056bdef9f10f0012
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-06-12 17:34:39 +0530

    Create 416.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

---
+++ prod_release_scripts/load/416.sql
@@ -0,0 +1,2 @@
+update meta_raw.process set usecase_id=259 where process_id=715;
+INSERT INTO META_RAW.USECASE (USECASE_ID, USECASE_NAME,CATEGORY,USECASE_DESCRIPTION,IS_ACTIVE,SEQUENCE_ID) VALUES (259,'gt_sellout_otc','GT_SELLOUT_OTC','gt_sellout_otc_desc','TRUE',1);
