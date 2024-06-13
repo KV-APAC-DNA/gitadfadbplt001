f029a103982b926abcb6f09f16395b7385f85f48
YMoger01 <158460502+YMoger01@users.noreply.github.com>
2024-06-14 02:26:42 +0530

    Update and rename 161.sql to 162.sql
    Signed-off-by: YMoger01 <158460502+YMoger01@users.noreply.github.com>

--- prod_release_scripts/core/161.sql
+++
@@ -1,3 +0,0 @@
-drop table prod_dna_core.thaitg_integration.itg_th_gt_route_insert_temp;
-
-drop table prod_dna_core.thaitg_integration.itg_th_gt_route_detail_insert_temp;
---
+++ prod_release_scripts/core/162.sql
@@ -0,0 +1,3 @@
+delete from prod_dna_core.idnitg_integration.sdl_raw_distributor_ivy_invoice
+where source_file_name = 'Invoice_20240606_0130.csv'
+and cdl_dttm = '2024-06-06 12:22:27';
