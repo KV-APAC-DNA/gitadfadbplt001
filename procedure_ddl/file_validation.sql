0f56d95fc1eb5f2fcbd03c376beb28503244a3a9
svavil01 <156428987+svavil01@users.noreply.github.com>
2024-06-14 14:07:23 +0100

    added handling for invalid utf8

--- procedure_ddl/file_validation.sql
+++ procedure_ddl/file_validation.sql
@@ -309,7 +309,7 @@ def main(session: snowpark.Session,Param):
             else:
                 file_name= CURRENT_FILE.replace("xlsx","csv").replace("xls","csv")
                 file_name = file_name.replace("(", "").replace(")", "").replace(" ","_")
-                df = session.read.option("INFER_SCHEMA", True).option("field_optionally_enclosed_by", "\\"").csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+                df = session.read.option("INFER_SCHEMA", True).option("field_optionally_enclosed_by", "\\"").option("REPLACE_INVALID_CHARACTERS",True).csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
             
                 df_pandas=df.to_pandas()
                 header=df_pandas.iloc[int(file_header_row_num)].tolist()
