dd5f7c2cda5278fff0869c122a1f1a802ae5988a
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-06-17 15:40:03 +0530

    Create 428.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

---
+++ prod_release_scripts/load/428.sql
@@ -0,0 +1,3 @@
+update parameters set parameter_value = 'Kenvue_Weekly_National_eCommerce_Data' where parameter_id = 15519;
+
+update parameters set parameter_value = 'MONTHLY' where parameter_id in (15525,15532);
