e524c3fe69325b96440aae9d059b3508f2dbaf44
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-06-24 22:46:49 +0530

    Corrected ADLS path for NA
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

---
+++ prod_release_scripts/load/454.sql
@@ -0,0 +1,5 @@
+update meta_raw.parameters set parameter_value='ecommerce_sellout/transaction/tca/' where parameter_id=14271
+
+update meta_raw.parameters set parameter_value='ecommerce_sellout/transaction/unitoa/' where parameter_id=14290
+
+update meta_raw.parameters set parameter_value='coupang_premium/transaction/' where parameter_id=11908
