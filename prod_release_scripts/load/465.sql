1d4302b51fd4e023e533864cff3a01f52c1548ed
dkanun02 <165705679+dkanun02@users.noreply.github.com>
2024-06-27 17:49:07 +0530

    Create 465.sql
    Signed-off-by: dkanun02 <165705679+dkanun02@users.noreply.github.com>

---
+++ prod_release_scripts/load/465.sql
@@ -0,0 +1,5 @@
+alter table PROD_DNA_LOAD.PCFSDL_RAW.SDL_PERENSO_HEAD_OFFICE_REQ_CHECK 
+alter notes set data type varchar(600);
+
+alter table PROD_DNA_LOAD.PCFSDL_RAW.SDL_RAW_PERENSO_HEAD_OFFICE_REQ_CHECK 
+alter notes set data type varchar(600);
