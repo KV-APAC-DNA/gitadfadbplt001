10ede87feefac2a4eade0a85e57ed0d29ca58a6e
GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>
2024-07-01 15:22:08 +0530

    Create 173.sql
    over and above points ITG layer change
    
    Signed-off-by: GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>

---
+++ prod_release_scripts/core/173.sql
@@ -0,0 +1,19 @@
+truncate table pcfitg_integration.itg_perenso_over_and_above_points;
+ 
+ 
+inset into pcfitg_integration.itg_perenso_over_and_above_points
+
+select 
+
+    display_type::varchar(256) as oa_display_type,
+
+    points::number(10,3) as points,
+
+    run_id::number(14,0) as run_id,
+
+    current_timestamp()::timestamp_ntz(9) as create_dt,
+
+    current_timestamp()::timestamp_ntz(9) as update_dt
+
+from PROD_DNA_LOAD.pcfsdl_raw.sdl_over_and_above_points;
+ 
