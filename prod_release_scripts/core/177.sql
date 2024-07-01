f1e1f59030dca922e4cbc941e25e0a6f7515a613
CDash6 <159747505+CDash6@users.noreply.github.com>
2024-07-01 21:06:57 +0530

    Create 177.sql
    Signed-off-by: CDash6 <159747505+CDash6@users.noreply.github.com>

---
+++ prod_release_scripts/core/177.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE ASPITG_INTEGRATION.VW_ITG_CUSTGP_CUSTOMER_HIERARCHY_SYNC
+(
+ctry_nm	text,
+cust_num	varchar(500),		--// character varying
+loc_channel1	varchar(500),		--// character varying
+loc_channel2	varchar(500),		--// character varying
+loc_channel3	text,
+loc_cust1	varchar(500),		--// character varying
+loc_cust2	text,
+loc_cust3	text,
+customer_segmentation	varchar(500),		--// character varying
+local_cust_segmentation	varchar(500),		--// character varying
+local_cust_segmentation_2	varchar(500)		--// character varying
+);
