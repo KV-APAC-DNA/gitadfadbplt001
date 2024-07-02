ac5202790dcdeec7952b172a63439014b475a890
svavil01 <156428987+svavil01@users.noreply.github.com>
2024-07-02 12:06:01 +0100

    changed to zip sources

--- procedure_ddl/HCP360_Procedure.sql
+++ procedure_ddl/HCP360_Procedure.sql
@@ -93,7 +93,9 @@ from snowflake.snowpark.types import IntegerType, StringType, StructType, Struct
 from datetime import datetime
 import snowflake.snowpark as snowpark
 import pytz
-
+import pandas as pd
+import zipfile
+from snowflake.snowpark.files import SnowflakeFile
 
 def main(session: snowpark.Session,Param):
     try:
@@ -102,26 +104,23 @@ def main(session: snowpark.Session,Param):
         temp_stage_path = Param[2]
         sch_name        = stage_name.split('.')[0]
         target_table    = sch_name+"."+Param[3]
-       
-        df = session.read\
-            .option("INFER_SCHEMA", True)\
-            .option("skip_header",0)\
-            .option("field_delimiter", "~")\
-            .option("field_optionally_enclosed_by", "\"") \
-            .option("REPLACE_INVALID_CHARACTERS",True) \
-            .option("trim_space", True) \
-            .with_metadata("METADATA$FILE_ROW_NUMBER") \
-            .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
 
-        df = df.filter(col("METADATA$FILE_ROW_NUMBER") > lit(1))
-        
-        df = df.drop("METADATA$FILE_ROW_NUMBER")
+        full_path = "@"+stage_name+"/"+temp_stage_path+"/"+file_name
+        pdf = pd.DataFrame()
+        with SnowflakeFile.open(full_path,'rb',require_scoped_url=False) as fhandle:
+            with zipfile.ZipFile(fhandle,"r") as zip_ref:
+            # Iterate over each file in the zip archive
+                for i_file_name in zip_ref.namelist():
+                    with zip_ref.open(i_file_name) as file:
+                        source_df = pd.read_csv(file, delimiter="~", header =0)
+                        pdf = pd.concat([pdf, source_df])
+                       
+        df= session.create_dataframe(pdf)
 
         df = df.withColumn("CRT_DTTM",lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S")))
         df = df.withColumn("FILE_NAME",lit(file_name).cast("string"))
         
         final_df = df
-
         if final_df.count()==0:
             return "No Data in file"
         
@@ -135,10 +134,10 @@ def main(session: snowpark.Session,Param):
         # write to success folder
     
         file_name=file_name.split(".")[0] + '_' + datetime.now().strftime("%Y%m%d%H%M%S")
-        final_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,file_format_type="csv",OVERWRITE=True,header=True)
+        final_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,file_format_type="csv",OVERWRITE=True,header=True,format_type_options=dict(FIELD_OPTIONALLY_ENCLOSED_BY="\""))
    
         return "Success"
-        
+
     except KeyError as key_error:
         
         error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
@@ -166,6 +165,9 @@ from snowflake.snowpark.types import IntegerType, StringType, StructType, Struct
 from datetime import datetime
 import snowflake.snowpark as snowpark
 import pytz
+import pandas as pd
+import zipfile
+from snowflake.snowpark.files import SnowflakeFile
 
 
 def main(session: snowpark.Session,Param):
@@ -175,27 +177,18 @@ def main(session: snowpark.Session,Param):
         temp_stage_path = Param[2]
         sch_name        = stage_name.split('.')[0]
         target_table    = sch_name+"."+Param[3]
-    
-        df_schema=StructType([
-            StructField("TEAM_NAME", StringType()),
-            StructField("V_SAMPLEID", StringType()),
-            StructField("V_EMPID", StringType()),
-            StructField("V_CUSTID", StringType()),
-            StructField("DCR_DT", StringType()),
-            StructField("SAMPLE_PRODUCT", StringType()),
-            StructField("SAMPLE_UNITS", StringType()),
-            StructField("CATEGORY", StringType())
-        ])
 
-
-        df = session.read\
-            .schema(df_schema)\
-            .option("skip_header",1)\
-            .option("field_delimiter", "~")\
-            .option("field_optionally_enclosed_by", None) \
-            .option("REPLACE_INVALID_CHARACTERS",True) \
-            .option("trim_space", True) \
-            .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+        full_path = "@"+stage_name+"/"+temp_stage_path+"/"+file_name
+        pdf = pd.DataFrame()
+        with SnowflakeFile.open(full_path,'rb',require_scoped_url=False) as fhandle:
+            with zipfile.ZipFile(fhandle,"r") as zip_ref:
+            # Iterate over each file in the zip archive
+                for i_file_name in zip_ref.namelist():
+                    with zip_ref.open(i_file_name) as file:
+                        source_df = pd.read_csv(file, delimiter="~", header =0)
+                        pdf = pd.concat([pdf, source_df])
+                       
+        df= session.create_dataframe(pdf)
 
         df = df.withColumn("CRT_DTTM",lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S")))
         df = df.withColumn("FILE_NAME",lit(file_name).cast("string"))
@@ -246,6 +239,9 @@ from snowflake.snowpark.types import IntegerType, StringType, StructType, Struct
 from datetime import datetime
 import snowflake.snowpark as snowpark
 import pytz
+import pandas as pd
+import zipfile
+from snowflake.snowpark.files import SnowflakeFile
 
 
 def main(session: snowpark.Session,Param):
@@ -255,54 +251,47 @@ def main(session: snowpark.Session,Param):
         temp_stage_path = Param[2]
         sch_name        = stage_name.split('.')[0]
         target_table    = sch_name+"."+Param[3]
-    
-        df_schema=StructType([
-            StructField("TEAM_NAME", StringType()),
-            StructField("V_CUSTID", StringType()),
-            StructField("V_TERRID", StringType()),
-            StructField("CUST_NAME", StringType()),
-            StructField("CUST_TYPE", StringType()),
-            StructField("CUST_QUAL", StringType()),
-            StructField("CUST_SPEC", StringType()),
-            StructField("CORE_NONCORE", StringType()),
-            StructField("CLASSIFICATION", StringType()),
-            StructField("IS_FBM_ADOPTED", StringType()),
-            StructField("VISITS_PER_MONTH", StringType()),
-            StructField("CELL_PHONE", StringType()),
-            StructField("PHONE", StringType()),
-            StructField("EMAIL", StringType()),
-            StructField("CITY", StringType()),
-            StructField("STATE", StringType()),
-            StructField("IS_ACTIVE", StringType()),
-            StructField("FIRST_RX_DATE", StringType()),
-            StructField("CUST_ENDTERED_DATE", StringType()),
-            StructField("Consent_Flag", StringType()),
-            StructField("Consent_Update_Datetime", StringType())
-        ])
 
-
-        df = session.read\
-            .schema(df_schema)\
-            .option("skip_header",1)\
-            .option("field_delimiter", "~")\
-            .option("field_optionally_enclosed_by", None) \
-            .option("REPLACE_INVALID_CHARACTERS",True) \
-            .option("trim_space", True) \
-            .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+        full_path = "@"+stage_name+"/"+temp_stage_path+"/"+file_name
+        pdf = pd.DataFrame()
+        with SnowflakeFile.open(full_path,'rb',require_scoped_url=False) as fhandle:
+            with zipfile.ZipFile(fhandle,"r") as zip_ref:
+            # Iterate over each file in the zip archive
+                for i_file_name in zip_ref.namelist():
+                    with zip_ref.open(i_file_name) as file:
+                        source_df = pd.read_csv(file, delimiter="~", header =0, encoding_errors = "replace", quoting=3, na_filter=False)
+                        for column in source_df.columns:
+                            source_df[column] = source_df[column].apply(str)
+                        pdf = pd.concat([pdf, source_df])
+                       
+        df= session.create_dataframe(pdf)
 
         df = df.withColumn("CRT_DTTM",lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S")))
         df = df.withColumn("FILE_NAME",lit(file_name).cast("string"))
+
+        df = df.withColumn("CONSENTFLAG", col('"Consent_Flag"'))
+        df = df.withColumn("CONSENTUPDATETIME", when(col('"Consent_Update_Datetime"') == lit(""), lit(None)).otherwise(col('"Consent_Update_Datetime"')))
+        df = df.withColumn("FIRSTRXDATE", when(col("FIRST_RX_DATE") == lit(""), lit(None)).otherwise(col("FIRST_RX_DATE")))
+        df = df.withColumn("CUSTENDTEREDDATE", when(col("CUST_ENDTERED_DATE") == lit(""), lit(None)).otherwise(col("CUST_ENDTERED_DATE")))
         
         final_df = df.select(
             "TEAM_NAME", "V_CUSTID", "V_TERRID", "CUST_NAME", "CUST_TYPE", "CUST_QUAL", "CUST_SPEC", "CORE_NONCORE", "CLASSIFICATION",\
-            "IS_FBM_ADOPTED", "VISITS_PER_MONTH", "CELL_PHONE", "PHONE", "EMAIL", "CITY", "STATE", "IS_ACTIVE", "FIRST_RX_DATE", \
-            "CRT_DTTM", "FILE_NAME", "CUST_ENDTERED_DATE", "Consent_Flag", \
-            to_timestamp("Consent_Update_Datetime", lit("MM/DD/YYYY HH12:MI:SS AM")).as_("Consent_Update_Datetime"), \
+            "IS_FBM_ADOPTED", "VISITS_PER_MONTH", "CELL_PHONE", "PHONE", "EMAIL", "CITY", "STATE", "IS_ACTIVE", "FIRSTRXDATE", \
+            "CRT_DTTM", "FILE_NAME", "CUSTENDTEREDDATE", "CONSENTFLAG", \
+            to_timestamp("CONSENTUPDATETIME", lit("MM/DD/YYYY HH12:MI:SS AM")).as_("CONSENTUPDATETIME"), \
 
         ).alias("final_df")
 
         if final_df.count()==0:
             return "No Data in file"
+
+        
+        file_df = df.select(
+            "TEAM_NAME", "V_CUSTID", "V_TERRID", "CUST_NAME", "CUST_TYPE", "CUST_QUAL", "CUST_SPEC", "CORE_NONCORE", "CLASSIFICATION",\
+            "IS_FBM_ADOPTED", "VISITS_PER_MONTH", "CELL_PHONE", "PHONE", "EMAIL", "CITY", "STATE", "IS_ACTIVE", "FIRST_RX_DATE", \
+            "CRT_DTTM", "FILE_NAME", "CUST_ENDTERED_DATE", '"Consent_Flag"', \
+            '"Consent_Update_Datetime"', \
+        ).alias("file_df")
         
         # Load Data to the target table
         final_df.write.mode("append").saveAsTable(target_table)
@@ -314,9 +303,10 @@ def main(session: snowpark.Session,Param):
         # write to success folder
     
         file_name=file_name.split(".")[0] + '_' + datetime.now().strftime("%Y%m%d%H%M%S")
-        final_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,file_format_type="csv",OVERWRITE=True,header=True)
+        file_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,file_format_type="csv",OVERWRITE=True,header=True,format_type_options=dict(FIELD_OPTIONALLY_ENCLOSED_BY="\""))
    
         return "Success"
+        
         
     except KeyError as key_error:
         
