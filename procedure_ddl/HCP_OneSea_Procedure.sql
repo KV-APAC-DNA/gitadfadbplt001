ca67612e51d7dd2cbc31b15432bedf481daa0857
svavil01 <156428987+svavil01@users.noreply.github.com>
2024-07-02 12:07:12 +0100

    onsea proc for iconnect added

--- procedure_ddl/HCP_OneSea_Procedure.sql
+++ procedure_ddl/HCP_OneSea_Procedure.sql
@@ -168,3 +168,78 @@ def main(session: snowpark.Session,Param):
         return error_message
 
 $$;
+
+
+
+
+CREATE OR REPLACE PROCEDURE HCPOSESDL_RAW.HCP_OSEA_SALESFORCE_LOAD_PREPROCESSING("PARAM" ARRAY)
+RETURNS VARCHAR(16777216)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+EXECUTE AS OWNER
+AS
+$$
+from snowflake.snowpark.functions import col, lit, date_format, current_timestamp, to_date, year, month, concat, format_number, regexp_replace,to_timestamp,when,trim,upper
+from snowflake.snowpark.types import IntegerType, StringType, StructType, StructField, DecimalType,DateType
+from datetime import datetime
+import snowflake.snowpark as snowpark
+import pytz
+
+
+def main(session: snowpark.Session,Param):
+    try:
+        file_name       = Param[0]
+        stage_name      = Param[1]
+        temp_stage_path = Param[2]
+        sch_name        = stage_name.split('.')[0]
+        target_table    = sch_name + "." + Param[3]
+        df = session.read\
+            .option("INFER_SCHEMA", True)\
+            .option("skip_header",0)\
+            .option("field_delimiter", "\u0001")\
+            .option("record_delimiter", "\r\n")\
+            .option("field_optionally_enclosed_by", None) \
+            .option("trim_space", True) \
+            .with_metadata("METADATA$FILE_ROW_NUMBER") \
+            .csv("@"+stage_name+"/"+temp_stage_path+"/"+file_name)
+
+        df = df.filter(col("METADATA$FILE_ROW_NUMBER") > lit(1))
+        
+        df = df.drop("METADATA$FILE_ROW_NUMBER")
+        
+        if df.count()==0:
+            return "No Data in file"
+
+        df = df.withColumn("CRT_DTTM",lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y-%m-%d %H:%M:%S")))
+        df = df.withColumn("FILE_NAME",lit(file_name).cast("string"))
+        df = df.withColumn("RUN_ID",lit(datetime.now(pytz.timezone("Asia/Singapore")).strftime("%Y%m%d%H%M%S")))
+    
+        final_df = df
+
+        # Load Data to the target table
+        final_df.write.mode("append").saveAsTable(target_table)
+
+        current_date = datetime.now()
+        formatted_year = current_date.strftime("%Y")
+        formatted_month = current_date.strftime("%m")
+
+        # write to success folder
+    
+        file_name=file_name.split(".")[0] + '_' + datetime.now().strftime("%Y%m%d%H%M%S")
+        final_df.write.copy_into_location("@"+stage_name+"/"+temp_stage_path+"/processed/success/"+formatted_year+"/"+formatted_month+"/"+file_name,file_format_type="csv",OVERWRITE=True,header=True)
+        
+        return "Success"
+        
+    except KeyError as key_error:
+        
+        error_message = f"KeyError: {str(key_error)}. Ensure all required columns are present in the DataFrame."
+        return error_message
+        
+    except Exception as e:
+        
+        error_message = f"Error: {str(e)}" +str(df.columns)
+        return error_message
+
+$$;
