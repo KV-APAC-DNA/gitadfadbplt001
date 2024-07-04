109865cde56c04f43e90864bce171bc1b3a40766
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-07-04 15:36:04 +0530

    Create 479.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/load/479.sql
@@ -0,0 +1,49 @@
+use schema meta_raw;
+
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (473,57,'NA','itx-arm-conapdna-korea-prod-kdp','raw-data-lake/otc_inventory/','OTC_KR_INV_1','kor','otc_inventory/transactional/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (474,57,'NA','itx-arm-conapdna-korea-prod-kdp','raw-data-lake/otc_inventory/','OTC_KR_INV_2','kor','otc_inventory/transactional/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (475,57,'NA','itx-arm-conapdna-korea-prod-kdp','raw-data-lake/otc_inventory/','OTC_KR_INV_3','kor','otc_inventory/transactional/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (476,57,'NA','itx-arm-conapdna-korea-prod-kdp','raw-data-lake/otc_inventory/','OTC_KR_INV_4','kor','otc_inventory/transactional/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (477,57,'NA','itx-arm-conapdna-korea-prod-kdp','raw-data-lake/otc_inventory/','OTC_KR_INV_5','kor','otc_inventory/transactional/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (478,58,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/Taiwan_Forecast/transaction/BP_Target/','BP','twn','taiwan_forecast/transaction/bp_target/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (479,58,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/Taiwan_Forecast/transaction/BU_Forecast/PROD_HIER/','BU','twn','taiwan_forecast/transaction/bu_forecast/prod_hier/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (480,58,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/Taiwan_Forecast/transaction/BU_Forecast/SKU/','BU','twn','taiwan_forecast/transaction/bu_forecast/sku/',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (481,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107479','TW_IMS_Distributor_107479_Customer','twn','ims/distributor/107479',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (482,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107485','TW_IMS_Distributor_107485_Customer','twn','ims/distributor/107485',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (483,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107501','TW_IMS_Distributor_107501_Customer','twn','ims/distributor/107501',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (484,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107507','TW_IMS_Distributor_107507_Customer','twn','ims/distributor/107507',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (485,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107510','TW_IMS_Distributor_107510_Customer','twn','ims/distributor/107510',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (486,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/116047','TW_IMS_Distributor_116047_Customer','twn','ims/distributor/116047',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (487,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/120812','TW_IMS_Distributor_120812_Customer','twn','ims/distributor/120812',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (488,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/122296','TW_IMS_Distributor_122296_Customer','twn','ims/distributor/122296',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (489,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/123291','TW_IMS_Distributor_123291_Customer','twn','ims/distributor/123291',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (490,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/131953','TW_IMS_Distributor_131953_Customer','twn','ims/distributor/131953',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (491,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/132349','TW_IMS_Distributor_132349_Customer','twn','ims/distributor/132349',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (492,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/132508','TW_IMS_Distributor_132508_Customer','twn','ims/distributor/132508',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (493,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/135307','TW_IMS_Distributor_135307_Customer','twn','ims/distributor/135307',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (494,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/135561','TW_IMS_Distributor_135561_Customer','twn','ims/distributor/135561',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (495,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107482','TW_IMS_Distributor_107482_Customer','twn','ims/distributor/107482',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (496,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/107483','TW_IMS_Distributor_107483_Customer','twn','ims/distributor/107483',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (497,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/132222','TW_IMS_Distributor_132222_Customer','twn','ims/distributor/132222',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (498,59,'NA','itx-arm-conapdna-taiwan-prod-kdp','raw-data-lake/ims/distributor/136454','TW_IMS_Distributor_136454_Customer','twn','ims/distributor/136454',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (499,43,'NA','itx-arm-conapdna-hongkong-prod-kdp','raw-data-lake/pos/Mannings','J_J','hkg','pos/Mannings',TRUE,'Y');
+INSERT INTO s3_to_adls(id,group_id,country,s3_bucket,s3_path,s3_file,adls_container,adls_path,Isactive,DELETE_SOURCE_FILE) VALUES (500,42,'NA','itx-arm-conapdna-korea-prod-kdp','raw-data-lake/pos/lalavla','KR_POS_Lalavla','kor','pos/lalavla',TRUE,'Y');
+
+update parameters set parameter_value ='/JnJ-TH/data/masterdata' where parameter_id = 14870;
+update parameters set parameter_value ='/JnJ-TH/data/masterdata' where parameter_id = 14877;
+update parameters set parameter_value ='/JnJ-TH/data/masterdata' where parameter_id = 14884;
+update parameters set parameter_value ='/JnJ-TH/data/masterdata' where parameter_id = 14849;
+update parameters set parameter_value ='/JnJ-TH/data/masterdata' where parameter_id = 14856;
+update parameters set parameter_value ='/JnJ-TH/data/masterdata' where parameter_id = 14890;
+update parameters set parameter_value ='sgp' where parameter_id = 18031;
+update parameters set parameter_value ='sgp' where parameter_id = 18038;
+update parameters set parameter_value ='sgp' where parameter_id = 18044;
+update parameters set parameter_value ='sgp' where parameter_id = 18048;
+update parameters set parameter_value ='sgp' where parameter_id = 18052;
+update parameters set parameter_value ='sgp' where parameter_id = 18059;
+update parameters set parameter_value ='sgp' where parameter_id = 18066;
+update parameters set parameter_value ='sgp' where parameter_id = 18073;
+
+update process 
+set source_id = 4
+where process_id in (1280,1682,1683,1684,1685,1686);
