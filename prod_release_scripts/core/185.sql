c60322cfb500c220f902bc20adf66c06c7042862
jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>
2024-07-05 17:19:29 +0530

    Create 185.sql
    Signed-off-by: jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>

---
+++ prod_release_scripts/core/185.sql
@@ -0,0 +1,2 @@
+delete from prod_dna_core.aspitg_integration.itg_copa_trans where file_name = 'SAP_BW_COPA10_20240705_022212.csv';
+delete from prod_dna_core.aspedw_integration.edw_copa_trans_fact where crt_dttm = '2024-07-05 04:06:19.924';
