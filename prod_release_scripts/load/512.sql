f1c027ff6d019aa217ba7c98f9d8a5717ea2bc41
GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>
2024-07-15 15:03:15 +0530

    Create 512.sql
    Signed-off-by: GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>

---
+++ prod_release_scripts/load/512.sql
@@ -0,0 +1,2 @@
+UPDATE PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE='SELECT * from delv.otif_d_cnsmr_attr_detl;' WHERE parameter_group_id=2001 AND PARAMETER_NAME='ms_query' and PARAMETER_ID=25008;
+
