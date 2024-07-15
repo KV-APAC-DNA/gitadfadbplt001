2cb5ba05df56d18a004eeaf766e6c349ea5e3fb0
GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>
2024-07-15 12:44:45 +0530

    Create 507.sql
    Signed-off-by: GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>

---
+++ prod_release_scripts/load/507.sql
@@ -0,0 +1,6 @@
+INSERT INTO PROD_DNA_LOAD.META_RAW.PROCESS 
+VALUES
+(2002,2002,'otif_etl_framework_ing',2002,1,1,FALSE,TRUE,501,null,'2',null,'','','','','','');
+INSERT INTO PROD_DNA_LOAD.META_RAW.PROCESS 
+VALUES
+(2003,2003,'otif_etl_framework_tran',2003,1,2,FALSE,TRUE,501,null,'3',null,'','','','','','');
