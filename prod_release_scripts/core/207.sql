38ad8c5f68c4f8fae4552c286a6495769323a4bd
AAgraw03 <148230421+AAgraw03@users.noreply.github.com>
2024-07-18 17:47:17 +0530

    Create 207.sql
    Signed-off-by: AAgraw03 <148230421+AAgraw03@users.noreply.github.com>

---
+++ prod_release_scripts/core/207.sql
@@ -0,0 +1,2 @@
+delete from NTAITG_INTEGRATION.ITG_KR_DADS_NAVER_GMV where file_name = 'Product_performance_weekly_20240617_20240623.csv';
+delete from NTAITG_INTEGRATION.ITG_KR_DADS_NAVER_SEARCH_CHANNEL where file_name = 'search_channel_20240527.csv';
