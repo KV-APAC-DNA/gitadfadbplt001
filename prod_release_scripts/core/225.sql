d620e96d0707951aaf1660cff79be4cf585b2b38
AAgraw03 <148230421+AAgraw03@users.noreply.github.com>
2024-07-29 17:08:00 +0530

    Update and rename 225.sql to 226.sql
    Signed-off-by: AAgraw03 <148230421+AAgraw03@users.noreply.github.com>

--- prod_release_scripts/core/225.sql
+++
@@ -1,1 +0,0 @@
-delete from prod_dna_core.ntaedw_integration.edw_ims_fact where dstr_cd='';
---
+++ prod_release_scripts/core/226.sql
@@ -0,0 +1,2 @@
+delete from ntaitg_integration.ITG_KR_ECOMMERCE_SELLOUT where customer_name='Emart' 
+and sub_customer_name='SSG.COM' and source_file_name='KR_POS_SSG_20240725.xls';
