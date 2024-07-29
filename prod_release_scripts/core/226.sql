4e236a519adab3ba3ad02663f5445e20f4c8bd3e
AAgraw03 <148230421+AAgraw03@users.noreply.github.com>
2024-07-29 17:46:34 +0530

    Update and rename 226.sql to 227.sql
    Signed-off-by: AAgraw03 <148230421+AAgraw03@users.noreply.github.com>

--- prod_release_scripts/core/226.sql
+++
@@ -1,2 +0,0 @@
-delete from ntaitg_integration.ITG_KR_ECOMMERCE_SELLOUT where customer_name='Emart' 
-and sub_customer_name='SSG.COM' and source_file_name='KR_POS_SSG_20240725.xls';
---
+++ prod_release_scripts/core/227.sql
@@ -0,0 +1,1 @@
+delete from select count(*) from NTAEDW_INTEGRATION.edw_ims_inventory_fact where dstr_cd='';
