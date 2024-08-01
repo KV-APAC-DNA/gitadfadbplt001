21bca2efdc2f9a5e29c51bb9305b1a3ec5178fb0
AAgraw03 <148230421+AAgraw03@users.noreply.github.com>
2024-08-01 10:48:18 +0530

    Update 233.sql
    Signed-off-by: AAgraw03 <148230421+AAgraw03@users.noreply.github.com>

--- prod_release_scripts/core/233.sql
+++ prod_release_scripts/core/233.sql
@@ -1,2 +1,2 @@
-delete from ntaitg_integration.itg_ims where crt_dttm='2024-07-31 17:58:38.705' and dstr_cd='110256' and ims_txn_dt like '2024-07%' group by 1,2;
+delete from ntaitg_integration.itg_ims where crt_dttm='2024-07-31 17:58:38.705' and dstr_cd='110256' and ims_txn_dt like '2024-07%';
 create or replace ntaitg_integration.itg_ims_temp clone ntaitg_integration.itg_ims;
