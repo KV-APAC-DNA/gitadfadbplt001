4350e6b0eaecf7a1cad07939095f1e9d565b317e
VasundharaU <vujawane@kenvue.com>
2024-08-09 14:54:46 +0530

    Create 281.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/281.sql
@@ -0,0 +1,5 @@
+create table phledw_integration.edw_ph_sellout_analysis_bkp as select * from phledw_integration.edw_ph_sellout_analysis;
+
+Drop table if exists phledw_integration.edw_ph_sellout_analysis;
+
+create table phledw_integration.edw_ph_sellout_analysis as select * from phledw_integration.edw_vw_ph_sellout_analysis;
