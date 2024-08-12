49785364bc8d004732728d6e9233b44b3db6b33a
VasundharaU <vujawane@kenvue.com>
2024-08-12 14:20:35 +0530

    Update 283.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

--- prod_release_scripts/core/283.sql
+++ prod_release_scripts/core/283.sql
@@ -1,4 +1,4 @@
 delete from phlitg_integration.itg_ph_tbl_surveyanswers where filename = 'ISE_tbl_SurveyAnswers_PH_20240804020004.csv'
 and run_id = '20240808023701';
 
-delete phlitg_integration.itg_ph_tbl_surveyanswers where filename = 'ISE_tbl_SurveyAnswers_PH_20240731020007.csv';
+delete from phlitg_integration.itg_ph_tbl_surveyanswers where filename = 'ISE_tbl_SurveyAnswers_PH_20240731020007.csv';
