8d5f7c0b9af5f0462e775935ecbbd372fa364709
AAgraw03 <148230421+AAgraw03@users.noreply.github.com>
2024-08-13 23:36:07 +0530

    Update and rename 295.sql to 296.sql
    Signed-off-by: AAgraw03 <148230421+AAgraw03@users.noreply.github.com>

--- prod_release_scripts/core/295.sql
+++
@@ -1,2 +0,0 @@
-delete from prod_dna_core.ntaitg_integration.itg_pos
-where src_sys_cd in ('Emart') and pos_dt='2024-08-12' and crt_dttm='2024-08-14 01:14:27.729';
---
+++ prod_release_scripts/core/296.sql
@@ -0,0 +1,2 @@
+delete from prod_dna_core.ntaitg_integration.itg_pos
+where src_sys_cd in ('Watsons 屈臣氏') and pos_dt='2024-07-19' and crt_dttm='2024-08-03 14:04:29.570';
