3f994858e7aa94651697677e241459d5125e2fc8
VasundharaU <vujawane@kenvue.com>
2024-08-19 15:38:32 +0530

    Create 311.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/311.sql
@@ -0,0 +1,4 @@
+
+alter table vnmitg_integration.sdl_raw_vn_dms_distributor_dim modify column DSTRBTR_ADDRESS varchar(500);
+
+alter table vnmitg_integration.itg_vn_dms_distributor_dim modify column DSTRBTR_ADDRESS varchar(500);
