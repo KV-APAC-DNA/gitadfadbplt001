bd9ce9f115c290609ce197f174ccd128221afc97
SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-08-19 10:50:42 +0530

    Update 307.sql
    Signed-off-by: SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>

--- prod_release_scripts/core/306.sql
+++ prod_release_scripts/core/306.sql
@@ -1,4 +1,6 @@
-delete from prod_dna_core.ntaitg_integration.itg_pos_temp
-where  src_sys_cd in ('Poya 寶雅') and pos_dt = '2024-08-16';
 delete from prod_dna_core.ntaitg_integration.itg_pos
-where  src_sys_cd in ('Poya 寶雅') and pos_dt = '2024-08-16';
+where  src_sys_cd in ('EC') 
+and (left(pos_dt,7) = '2024-07');
+delete from prod_dna_core.ntaitg_integration.itg_pos_temp
+where  src_sys_cd in ('EC') 
+and (left(pos_dt,7) = '2024-07');
