90c5c74171e8553f174e26e78749f6cb74ee0b92
SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-08-19 12:26:37 +0530

    Update and rename 308.sql to 309.sql
    Signed-off-by: SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>

--- prod_release_scripts/core/308.sql
+++
@@ -1,6 +0,0 @@
-delete from prod_dna_core.ntaitg_integration.itg_pos
-where  src_sys_cd in ('Poya 寶雅') 
-and pos_dt = '2024-08-16';
-delete from prod_dna_core.ntaitg_integration.itg_pos_temp
-where  src_sys_cd in ('Poya 寶雅') 
-and pos_dt = '2024-08-16';
---
+++ prod_release_scripts/core/309.sql
@@ -0,0 +1,7 @@
+update prod_dna_core.ntaitg_integration.itg_ims
+set prod_cd = '79611705/79633161'
+where prod_cd = '79611705/796331';
+
+update prod_dna_core.ntaitg_integration.itg_ims
+set prod_cd = '158263332/79633283'
+where prod_cd = '158263332/79633';
