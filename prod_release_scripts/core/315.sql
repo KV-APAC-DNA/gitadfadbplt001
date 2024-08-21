6cfab6fccbc48962c2be0bc3a9de9b577ae710ce
SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-08-21 15:31:43 +0530

    Update 315.sql
    Signed-off-by: SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>

--- prod_release_scripts/core/315.sql
+++ prod_release_scripts/core/315.sql
@@ -1,3 +1,2 @@
 delete from prod_dna_core.ntaitg_integration.ITG_KR_GT_SELLOUT
-where ctry_cd = 'KR' and dstr_cd = 'OTC' 
-and crt_dttm = '2024-08-21 14:24:19.923';
+where ctry_cd = 'KR' and dstr_cd = 'OTC' and ims_txn_dt like '2024-07%';
