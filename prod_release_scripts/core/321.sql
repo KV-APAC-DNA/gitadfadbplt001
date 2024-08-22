2f31fb6f785f5f599b33ff0fda063e87cc60fda2
VasundharaU <vujawane@kenvue.com>
2024-08-22 14:21:28 +0530

    Update 321.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

--- prod_release_scripts/core/321.sql
+++ prod_release_scripts/core/321.sql
@@ -2,3 +2,6 @@
 
  Insert into prod_dna_core.dbt_cloud_pr_5458_963.aspitg_integration__itg_pop6_product_attribute_audits 
  select * from aspitg_integration.itg_pop6_product_attribute_audits where cntry_cd = 'TH';
+
+delete from prod_dna_core.dbt_cloud_pr_5458_963.aspitg_integration__itg_pop6_sku_audits where cntry_cd = 'TH'
+ and src_file_date in ('20240816', '20240817','20240818','20240819','20240820','20240821');
