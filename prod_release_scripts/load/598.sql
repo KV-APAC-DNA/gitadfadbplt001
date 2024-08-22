c35f0749699966eaa198322ab016c41bc5bf1666
VasundharaU <vujawane@kenvue.com>
2024-08-22 12:42:44 +0530

    Create 598.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/load/598.sql
@@ -0,0 +1,2 @@
+delete from meta_raw.process where process_id in (2187, 2188, 2189, 2190, 2191, 2192, 2193, 2194);
+delete from meta_raw.parameters where parameter_group_id in (2187, 2186, 2189, 2190, 2191, 2192, 2193, 2194);
