c6b38d30090dd3cb626cb11211b738aa96026880
VasundharaU <vujawane@kenvue.com>
2024-08-27 12:53:46 +0530

    Create 328.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/328.sql
@@ -0,0 +1,3 @@
+alter table phlitg_integration.itg_ph_tbl_surveyanswers modify column BRANCHCODE varchar(100);
+alter table phlitg_integration.itg_ph_tbl_surveycustomers modify column BRANCHCODE varchar(100);
+alter table phlitg_integration.itg_ph_tbl_surveynotes modify column BRANCHCODE varchar(100);
