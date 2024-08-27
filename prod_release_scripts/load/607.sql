a94ce77fe8de15939e3e29feab6660b6f9ab26ac
VasundharaU <vujawane@kenvue.com>
2024-08-27 12:52:31 +0530

    Create 607.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/load/607.sql
@@ -0,0 +1,3 @@
+alter table phlsdl_raw.SDL_PH_TBL_SURVEYNOTES modify column BRANCHCODE varchar(100);
+alter table phlsdl_raw.SDL_PH_TBL_SURVEYCUSTOMERS modify column BRANCHCODE varchar(100);
+alter table phlsdl_raw.SDL_PH_TBL_SURVEYANSWERS modify column BRANCHCODE varchar(100);
