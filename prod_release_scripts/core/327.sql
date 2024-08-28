44652a436305448f82e72b9ccb86a7d0e8ed4dfa
SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-08-28 09:37:38 +0530

    Update and rename 327.sql to 329.sql
    Signed-off-by: SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>

--- prod_release_scripts/core/327.sql
+++
@@ -1,3 +0,0 @@
-delete from prod_dna_core.ntaitg_integration.itg_pos
-where src_sys_cd = 'Emart' and pos_dt is null 
- and crt_dttm in ('2024-08-27 11:27:01.583','2024-08-27 07:57:34.139');
---
+++ prod_release_scripts/core/329.sql
@@ -0,0 +1,2 @@
+delete from prod_dna_core.ntaitg_integration.itg_ims
+where crt_dttm in ('2024-08-26 19:58:03.696','2024-08-27 17:39:37.674');
