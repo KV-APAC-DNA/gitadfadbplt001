4a694a0c3b9f20e86d2e09ddc265fafd5f312b90
GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>
2024-08-30 08:22:22 +0530

    Create 620.sql
    MY GT MDS Metadata update
    
    Signed-off-by: GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>

---
+++ prod_release_scripts/load/620.sql
@@ -0,0 +1,15 @@
+UPDATE META_RAW.PARAMETERS 
+SET PARAMETER_VALUE = 'SELECT DISTRIBUTOR_ID as "Distributor_ID",SALES_ORDER_NUMBER as "Sales_Order_Number",SALES_ORDER_DATE as "Sales_Order_Date",
+TYPE as "Type",CUSTOMER_CODE as "Customer_Code",DISTRIBUTOR_WH_ID as "Distributor_WH_ID",SAP_MATERIAL_ID as "SAP_Material_ID",
+PRODUCT_CODE as "Product_Code",PRODUCT_EAN_CODE as "Product_EAN_Code",PRODUCT_DESCRIPTION as "Product_Description",GROSS_ITEM_PRICE as "Gross_Item_Price",QUANTITY as "Quantity",UOM as "UOM",QUANTITY_IN_PIECES as "Quantity_In_Pieces",QUANTITY_AFTER_CONVERSION as "Quantity_After_Conversion",SUB_TOTAL_1 as "Sub_Total_1",
+DISCOUNT as "Discount",SUB_TOTAL_2 as "Sub_Total_2",BOTTOM_LINE_DISCOUNT as "Bottom_Line_Discount",TOTAL_AMT_AFTER_TAX as "Total_Amt_After_Tax",TOTAL_AMT_BEFORE_TAX as "Total_Amt_Before_Tax",SALES_EMPLOYEE as "Sales_Employee",CRT_DTTM as "Insert_dt",
+''N'' as "ProcessedToMDS" FROM MYSEDW_ACCESS.SDL_VW_MY_DAILY_SELLOUT_SALES_FACT_MDS_SYNC'
+WHERE PARAMETER_ID = 27028;
+
+UPDATE META_RAW.PARAMETERS 
+SET PARAMETER_VALUE = 'SELECT DISTRIBUTOR_ID as "Distributor_ID",SALES_ORDER_NUMBER as "Sales_Order_Number",SALES_ORDER_DATE as "Sales_Order_Date",
+TYPE as "Type",CUSTOMER_CODE as "Customer_Code",DISTRIBUTOR_WH_ID as "Distributor_WH_ID",SAP_MATERIAL_ID as "SAP_Material_ID",
+PRODUCT_CODE as "Product_Code",PRODUCT_EAN_CODE as "Product_EAN_Code",PRODUCT_DESCRIPTION as "Product_Description",GROSS_ITEM_PRICE as "Gross_Item_Price",QUANTITY as "Quantity",UOM as "UOM",QUANTITY_IN_PIECES as "Quantity_In_Pieces",QUANTITY_AFTER_CONVERSION as "Quantity_After_Conversion",SUB_TOTAL_1 as "Sub_Total_1",
+DISCOUNT as "Discount",SUB_TOTAL_2 as "Sub_Total_2",BOTTOM_LINE_DISCOUNT as "Bottom_Line_Discount",TOTAL_AMT_AFTER_TAX as "Total_Amt_After_Tax",TOTAL_AMT_BEFORE_TAX as "Total_Amt_Before_Tax",SALES_EMPLOYEE as "Sales_Employee",CRT_DTTM as "Insert_dt",
+''N'' as "ProcessedToMDS" FROM MYSEDW_ACCESS.SDL_VW_MY_MONTHLY_SELLOUT_SALES_FACT_MDS_SYNC'
+WHERE PARAMETER_ID = 27020;
