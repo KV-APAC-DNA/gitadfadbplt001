2ca045bebeaa781b3ecfac9d7c8de654296d13f2
jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>
2024-09-04 16:34:55 +0530

    Create 332.sql
    Signed-off-by: jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>

---
+++ prod_release_scripts/core/332.sql
@@ -0,0 +1,8 @@
+ALTER TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot 
+ADD COLUMN gros_trd_sls2 number(38,7);
+
+ALTER TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot 
+ADD COLUMN CNFRM_QTY_PC number(38,7);
+
+ALTER TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot 
+ADD COLUMN FUT_SLS_QTY number(38,7);
