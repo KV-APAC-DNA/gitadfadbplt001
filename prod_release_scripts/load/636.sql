7f06df266c4adb5c7f3dadae65b2294ad4e1188d
dkanun02 <165705679+dkanun02@users.noreply.github.com>
2024-09-05 11:23:53 +0530

    Create 636.sql
    Signed-off-by: dkanun02 <165705679+dkanun02@users.noreply.github.com>

---
+++ prod_release_scripts/load/636.sql
@@ -0,0 +1,4 @@
+ COPY INTO prod_DNA_LOAD.PHLSDL_RAW.sdl_pos_rks_rose_pharma
+  FROM @prod_DNA_LOAD.PHLSDL_RAW.prod_LOAD_STAGE_ADLS/prd/rpharma_mds/Rose_Pharma_OTC_Data
+FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ','  SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '\"' );
+
