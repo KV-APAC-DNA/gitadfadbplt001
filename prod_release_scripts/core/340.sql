d92ccf23a63055f85d1b4488cea6a049ffd6b7f3
VasundharaU <vujawane@kenvue.com>
2024-09-06 13:51:08 +0530

    Create 340.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/340.sql
@@ -0,0 +1,11 @@
+update  aspitg_integration.itg_pop6_displays a set field_code = 'Local_DISPLAY_QN_YESNO'
+from 
+aspitg_integration.itg_pop6_executed_visits exe,
+aspitg_integration.itg_pop6_pops pops
+where exe.visit_id = a.visit_id 
+and exe.visit_date like '2024-02%'
+and pops.popdb_id = exe.popdb_id
+and pops.active = 'Y'
+and a.cntry_cd = 'TH'
+and upper(pops.customer) in ('BIG C', 'GT SUPER' , 'GT SUPER VIP' , 'LOTUS''S' , 'THE MALL' , 'TOPS', 'WATSONS')
+and field_code = 'PS_DISPLAY_QN_YESNO';
