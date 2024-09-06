61f5f59f7ee9e9bd3da598b76ea9be41f96bed05
VasundharaU <vujawane@kenvue.com>
2024-09-06 13:53:41 +0530

    Update 340.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

--- prod_release_scripts/core/340.sql
+++ prod_release_scripts/core/340.sql
@@ -1,3 +1,7 @@
+delete from aspitg_integration.itg_pop6_displays;
+
+insert into aspitg_integration.itg_pop6_displays select * from aspitg_integration.itg_pop6_displays_bkp_20240906;
+
 update  aspitg_integration.itg_pop6_displays a set field_code = 'Local_DISPLAY_QN_YESNO'
 from 
 aspitg_integration.itg_pop6_executed_visits exe,
@@ -9,3 +13,13 @@ and pops.active = 'Y'
 and a.cntry_cd = 'TH'
 and upper(pops.customer) in ('BIG C', 'GT SUPER' , 'GT SUPER VIP' , 'LOTUS''S' , 'THE MALL' , 'TOPS', 'WATSONS')
 and field_code = 'PS_DISPLAY_QN_YESNO';
+
+update aspitg_integration.itg_pop6_displays a 
+set field_code = 'Local_DISPLAY_QN_YESNO'
+from aspitg_integration.itg_pop6_displays b
+ where a.visit_id  = b.visit_id 
+ and  b.field_label = 'เป็นร้านค้าที่บริษัทฯ มีการเช่าพื้นที่หรือไม่'  
+ and b.field_code = 'PS_DISPLAY_QN_YESNO'
+and upper(b.response) = 'NO'
+and a.field_code = 'PS_DISPLAY_QN_YESNO'
+;
