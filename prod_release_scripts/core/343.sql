eed0ad700a6556e8acba45f3b3c399d3b4107170
jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>
2024-09-09 11:00:44 +0530

    Create 343.sql
    Signed-off-by: jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>

---
+++ prod_release_scripts/core/343.sql
@@ -0,0 +1,14 @@
+DROP TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot;
+CREATE TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot CLONE PROD_DNA_CORE.pcfedw_integration.edw_invoice_fact_snapshot;
+
+ALTER TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot 
+ADD COLUMN gros_trd_sls2 number(38,7);
+ALTER TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot 
+ADD COLUMN CNFRM_QTY_PC number(38,7);
+ALTER TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_invoice_fact_snapshot 
+ADD COLUMN ord_qty_pc number(38,7);
+
+-------------------------
+drop table PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_sls_evolution;
+CREATE TABLE PROD_DNA_CORE.dbt_cloud_pr_5458_593.pcfedw_integration__edw_sls_evolution CLONE
+PROD_DNA_CORE.pcfedw_integratioN.edw_sls_evolution;
