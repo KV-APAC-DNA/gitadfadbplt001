7f343411ce3b6c3d4f7149cb9b2356289268392d
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-09-09 13:19:39 +0530

    Create 344.sql
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/core/344.sql
@@ -0,0 +1,2 @@
+alter table PROD_DNA_CORE.PHLITG_INTEGRATION.ITG_PH_DMS_SELLOUT_SALES_FACT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PHLITG_INTEGRATION.ITG_PH_DMS_SELLOUT_STOCK_FACT add column file_name varchar(255);
