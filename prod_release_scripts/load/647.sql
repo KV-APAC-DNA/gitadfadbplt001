53a1447ab74694c57486ce17dbb8639ab36ea7aa
GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>
2024-09-10 14:18:02 +0530

    Create 647.sql
    MDS Changes on Reverse Sync edw_rpt_regional_sellout_offtake
    
    Signed-off-by: GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>

---
+++ prod_release_scripts/load/647.sql
@@ -0,0 +1,16 @@
+update META_RAW.PARAMETERS
+set parameter_group_name = 'edw_rpt_regional_sellout_offtake_CN_group'
+where parameter_group_id = '2233';
+update META_RAW.PARAMETERS
+set parameter_value = 'edw_rpt_regional_sellout_offtake_CN'
+where parameter_id = '27283';
+update META_RAW.PARAMETERS
+set parameter_value = 'sql_server/MDS_Reverse_Sync/edw_rpt_regional_sellout_offtake_CN/'
+where parameter_id = '27282';;
+update META_RAW.PARAMETERS
+set parameter_value = 'dbo.RS_rg_edw_rpt_regional_sellout_offtake_CN'
+where parameter_id = '27284';
+
+update META_RAW.PROCESS
+set process_name = 'edw_rpt_regional_sellout_offtake_CN'
+where process_id = '2233';
