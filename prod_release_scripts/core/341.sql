61cb2d6a4902613650431f91ea8366f9b1b417f9
SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-09-10 19:28:16 +0530

    Update 341.sql
    Signed-off-by: SwethaNenavath96 <155425374+SwethaNenavath96@users.noreply.github.com>

--- prod_release_scripts/core/341.sql
+++ prod_release_scripts/core/341.sql
@@ -1,2 +1,2 @@
 delete from prod_dna_core.ntaitg_integration.itg_ims
-where crt_dttm = '2024-09-04 08:10:17.388' and dstr_cd = '110256' and ims_txn_dt in ('2024-08-29','2024-08-30');
+where crt_dttm = '2024-09-04 17:29:41.251' and dstr_cd = '110256' and ims_txn_dt in ('2024-08-29','2024-08-30');
