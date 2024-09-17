2f5b0a0cfecf70e71c0bfdc922b8544fa2208c89
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-09-17 15:17:53 +0530

    Create 375.sql
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/core/375.sql
@@ -0,0 +1,8 @@
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_in_iqvia_indication add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_click_data add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_forward_data add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_open_data add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_sent_data add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_hcp_details add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_unsubscribe_data add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPITG_INTEGRATION.itg_hcp360_sfmc_bounce_data add column file_name varchar(255);
