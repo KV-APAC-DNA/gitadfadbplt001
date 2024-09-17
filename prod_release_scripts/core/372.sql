6af1c1745d963ecf8393133e124c9c7f375c9726
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-09-17 12:23:27 +0530

    Create 372.sql
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/core/372.sql
@@ -0,0 +1,88 @@
+-- Pacific alter statements
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_CALL_OBJECTIVES add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_DIARY_ITEM add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_DIARY_ITEM_TIME add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_HEAD_OFFICE_REQ_CHECK add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_HEAD_OFFICE_REQ_STATE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_ORDER_BATCH add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_ORDER_DETAIL add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_ORDER_HEADER add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_OVER_AND_ABOVE_STATE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_PROD_CHK_DISTRIBUTION add column file_name varchar(255);
+
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_STORE_CHK_HDR add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_SURVEY_RESULT add column file_name varchar(255);
+
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_API_DSTR_RAW add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_DSTR_COLES_INV_RAW add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_DSTR_WOOLWORTH_INV_RAW add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_IG_INVENTORY_DATA_RAW add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_OVER_AND_ABOVE_POINTS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PACIFIC_PERENSO_CALL_COVERAGE_TARGETS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_CUSTOM_LIST add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_FIELDS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_GROUP add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_GROUP_LVL add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_GROUP_RELN add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_RELN_ID add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCOUNT_TYPE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ACCT_DIST_ACCT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_CALL_OBJECTIVES add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_CONSTANTS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_DEAL_DISCOUNT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_DIARY_ITEM add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_DIARY_ITEM_TIME add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_DIARY_ITEM_TYPE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_DISTRIBUTOR_DETAIL add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_HEAD_OFFICE_REQ_CHECK add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ORDER_BATCH add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ORDER_DETAIL add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ORDER_HEADER add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_ORDER_TYPE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_OVER_AND_ABOVE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_OVER_AND_ABOVE_STATE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PROD_BRANCH_IDENTIFIER add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PROD_CHK_DISTRIBUTION add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PRODUCT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PRODUCT_FIELDS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PRODUCT_GROUP add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PRODUCT_GROUP_LVL add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PRODUCT_GROUP_RELN add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_PRODUCT_RELN_ID add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_RANGING add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_RANGING_ACCT_GRP_REL add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_RANGING_PRODUCT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_STORE_CHK_HDR add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_SURVEY_RESULT add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_TODO_OPTION add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_USERS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_WORK_ITEM add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_SURVEY_PRODUCT_GRP_TO_CATEGORY_MAP add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_SURVEY_TARGETS add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_SURVEY_TYPE_TO_QUESTION_MAP add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_SYMBION_DSTR_RAW add column file_name varchar(255);
+
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_HEAD_OFFICE_REQ_STATE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.ITG_PERENSO_OVER_AND_ABOVE add column file_name varchar(255);
+alter table PROD_DNA_CORE.PCFITG_INTEGRATION.SDL_RAW_PERENSO_TODO add column file_name varchar(255);
+
+
+-- Japan
+alter table PROD_DNA_CORE.JPNITG_INTEGRATION.JP_POS_DAILY add column file_name varchar(255);
+
+--japan_dcl
+
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.CONTACT_HIST add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFCC_PRODUCT_CATEGORY add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFCC_PRODUCT_MST add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_BOUNCES add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_CLICKS add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_KOKYA_SUBSKEY_MAPPING add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_NOTSENT add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_OPENS add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_SENDJOBS add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_SENT add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.SFMC_UNSUBS add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.EXTRACTION_TABLE add column file_name varchar(255);
+alter table PROD_DNA_CORE.JPDCLITG_INTEGRATION.KR_SPECIAL_DISCOUNT_FILE add column file_name varchar(255);
