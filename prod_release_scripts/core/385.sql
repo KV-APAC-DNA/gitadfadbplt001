330669cc11a4ebd71c6e7c35c9da58a9f6f7ca20
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-09-19 13:20:05 +0530

    Create 385.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/385.sql
@@ -0,0 +1,5 @@
+alter table prod_dna_core.ntaitg_integration.sdl_raw_kr_dongbu_lsd_gt_sellout add column file_name varchar(255);
+alter table prod_dna_core.ntaitg_integration.sdl_raw_kr_bo_young_jong_hap_logistics_gt_sellout add column file_name varchar(255);
+alter table prod_dna_core.ntaitg_integration.sdl_raw_kr_hyundai_gt_sellout add column file_name varchar(255);
+alter table prod_dna_core.ntaitg_integration.sdl_raw_kr_da_in_sang_sa_gt_sellout add column file_name varchar(255);
+alter table prod_dna_core.ntaitg_integration.sdl_raw_kr_jungseok_gt_sellout add column file_name varchar(255);
