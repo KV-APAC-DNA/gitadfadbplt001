0e9882dbc89027ea20a601be040881468ae89916
dkanun02 <165705679+dkanun02@users.noreply.github.com>
2024-09-19 20:30:11 +0530

    Create 388.sql
    Signed-off-by: dkanun02 <165705679+dkanun02@users.noreply.github.com>

---
+++ prod_release_scripts/core/388.sql
@@ -0,0 +1,5 @@
+  delete 
+from PROD_DNA_CORE.PCFEDW_INTEGRATION.EDW_DEMAND_FORECAST_SNAPSHOT_temp
+WHERE PAC_SUBSOURCE_TYPE  in ('PX_FORECAST')
+and to_char(snap_shot_dt, 'YYYYMM') =202403
+and jj_period < 201803;
