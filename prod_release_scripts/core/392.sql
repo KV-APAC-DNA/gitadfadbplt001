8f2f5d1c72a15d8719621335cd661547e2ed249f
tsheka01 <155425332+tsheka01@users.noreply.github.com>
2024-09-20 12:51:21 +0530

    Create 392.sql
    Signed-off-by: tsheka01 <155425332+tsheka01@users.noreply.github.com>

---
+++ prod_release_scripts/core/392.sql
@@ -0,0 +1,3 @@
+alter table PROD_DNA_CORE.HCPOSEITG_INTEGRATION.ITG_ISIGHT_LICENSES add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPOSEITG_INTEGRATION.ITG_ISIGHT_SECTOR_MAPPING add column file_name varchar(255);
+alter table PROD_DNA_CORE.HCPOSEITG_INTEGRATION.ITG_HOLIDAY_LIST add column file_name varchar(255);
