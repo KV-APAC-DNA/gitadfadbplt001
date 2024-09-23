99688e82bda9105fd5f421254feda586caca4f13
AMAKHLOG <amakhlog@kenvue.com>
2024-09-23 20:19:10 +0530

    Update 408.sql
    Signed-off-by: AMAKHLOG <amakhlog@kenvue.com>

--- prod_release_scripts/core/408.sql
+++ prod_release_scripts/core/408.sql
@@ -1,4 +1,4 @@
 
 delete from PROD_DNA_CORE.PCFEDW_INTEGRATION.EDW_DEMAND_FORECAST_SNAPSHOT_temp
 where pac_subsource_type='SAPBW_ACTUAL'
-and to_char(snap_shot_dt, 'YYYYMM') in (202403,202404,202405);
+and to_char(snap_shot_dt, 'YYYYMM') = 202403;
