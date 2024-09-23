e53798657c6d5f51007a93d11fad2784b66d51e7
dkanun02 <165705679+dkanun02@users.noreply.github.com>
2024-09-23 17:57:06 +0530

    Update 407.sql
    Signed-off-by: dkanun02 <165705679+dkanun02@users.noreply.github.com>

--- prod_release_scripts/core/407.sql
+++ prod_release_scripts/core/407.sql
@@ -1,5 +1,5 @@
 
 
 delete from PROD_DNA_CORE.PCFEDW_INTEGRATION.EDW_DEMAND_FORECAST_SNAPSHOT_temp
-where ac_subsource_type='SAPBW_ACTUAL'
+where pac_subsource_type='SAPBW_ACTUAL'
 and to_char(snap_shot_dt, 'YYYYMM') in (202403,202404,202405);
