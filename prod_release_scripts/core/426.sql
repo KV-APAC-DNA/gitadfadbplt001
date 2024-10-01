c7c7e9ccd33c2c39c8426a4841f08b38c773a1c6
VasundharaU <vujawane@kenvue.com>
2024-10-01 17:40:38 +0530

    Update 426.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

--- prod_release_scripts/core/426.sql
+++ prod_release_scripts/core/426.sql
@@ -2,4 +2,4 @@ create table aspitg_integration.itg_pop6_products_bkp_20241001 as select * fro a
 
 create table aspitg_integration.itg_pop6_products_test as select * from aspitg_integration.itg_pop6_products;
 
-truncate table aspitg_integration.itg_pop6_products_test;
+delete from aspitg_integration.itg_pop6_products_test;
