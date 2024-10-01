9326aed86e53ecac1b12bae72d323d516fcc65fe
VasundharaU <vujawane@kenvue.com>
2024-10-01 17:43:22 +0530

    Update and rename 426.sql to 427.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

--- prod_release_scripts/core/427.sql
+++ prod_release_scripts/core/427.sql
@@ -1,4 +1,4 @@
-create table aspitg_integration.itg_pop6_products_bkp_20241001 as select * fro aspitg_integration.itg_pop6_products;
+create table aspitg_integration.itg_pop6_products_bkp_20241001 as select * from aspitg_integration.itg_pop6_products;
 
 create table aspitg_integration.itg_pop6_products_test as select * from aspitg_integration.itg_pop6_products;
 
