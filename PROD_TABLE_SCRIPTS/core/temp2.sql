6eb38a2ddf19a5d54b001bae2df98973640ee81e
SBugad01 <149662968+SBugad01@users.noreply.github.com>
2024-10-01 18:42:06 +0530

    Update temp2.sql
    Signed-off-by: SBugad01 <149662968+SBugad01@users.noreply.github.com>

--- PROD_TABLE_SCRIPTS/core/temp2.sql
+++ PROD_TABLE_SCRIPTS/core/temp2.sql
@@ -1,1 +1,2 @@
-drop table PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_WEEKLY_FORECAST;
+create table pcfitg_integration.itg_customer_sellout_bkp_10012024 as select * from pcfitg_integration.itg_customer_sellout;
+delete from pcfitg_integration.itg_customer_sellout where nullif(dstr_prod_cd,'') is null and nullif(matl_num,'') is null and nullif(ean,'') is null and SAP_PARENT_CUSTOMER_DESC='SYMBION';
