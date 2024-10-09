28cdff699a1d1e7f85b480581622279e5ca7b90d
mmadhu1988 <mmadhu@kenvue.com>
2024-10-09 23:03:26 +0530

    Create 751.sql
    Signed-off-by: mmadhu1988 <mmadhu@kenvue.com>

---
+++ prod_release_scripts/load/751.sql
@@ -0,0 +1,2 @@
+update prod_dna_load.meta_raw.parameters set parameter_value = 'PHLSDL_RAW.PH_NON_POS_ROBINSONS_SM_PREPROCESSING_TEST'
+where parameter_group_id = 5089 and parameter_name = 'sp_name';
