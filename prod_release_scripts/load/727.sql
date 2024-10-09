7ffc9e1dc509713f700e7c29175dce2d6ca04afd
GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>
2024-10-09 10:10:25 +0530

    Create 727.sql
    ID MDS QA Test
    
    Signed-off-by: GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>

---
+++ prod_release_scripts/load/727.sql
@@ -0,0 +1,3 @@
+update META_RAW.PARAMETERS
+SET PARAMETER_VALUE = 'SELECT key_outlet  AS "key_outlet",jj_sap_dstrbtr_id  AS "jj_sap_dstrbtr_id",jj_sap_dstrbtr_nm  AS "jj_sap_dstrbtr_nm",cust_id  AS "cust_id",cust_nm  AS "cust_nm",address  AS "address",city  AS "city",cust_grp  AS "cust_grp",chnl  AS "chnl",outlet_type  AS "outlet_type",chnl_grp  AS "chnl_grp",jjid  AS "jjid",pst_cd  AS "pst_cd",cust_id_map  AS "cust_id_map",cust_nm_map  AS "cust_nm_map",chnl_grp2  AS "chnl_grp2",cust_crtd_dt  AS "cust_crtd_dt",cust_grp2  AS "cust_grp2",crtd_dttm  AS "crtd_dttm",updt_dttm  AS "updt_dttm" FROM dbt_cloud_pr_5458_1401.idnitg_integration__sdl_distributor_ivy_outlet_master_mds_sync '
+WHERE PARAMETER_ID = 27012;
