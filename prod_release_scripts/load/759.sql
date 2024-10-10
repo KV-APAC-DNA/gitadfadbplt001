fd569dd7c70d7c57bf34a7ecce8e6443b2e40e68
mmadhu1988 <mmadhu@kenvue.com>
2024-10-10 17:43:29 +0530

    Create 759.sql
    Signed-off-by: mmadhu1988 <mmadhu@kenvue.com>

---
+++ prod_release_scripts/load/759.sql
@@ -0,0 +1,20 @@
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/svi_smc/ph_non_pos_svi_smc'
+where parameter_name = 'folder_path' and parameter_group_id = '5080';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/waltermart/ph_non_pos_waltermart'
+where parameter_name = 'folder_path' and parameter_group_id = '5081';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/landmark_sm/ph_non_pos_landmark_sm/'
+where parameter_name = 'folder_path' and parameter_group_id = '5082';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/rustans/ph_non_pos_rustans/'
+where parameter_name = 'folder_path' and parameter_group_id = '5083';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/puregold/ph_non_pos_puregold'
+where parameter_name = 'folder_path' and parameter_group_id = '5084';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/landmark_ds/ph_non_pos_landmark_ds'
+where parameter_name = 'folder_path' and parameter_group_id = '5085';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/shm/ph_non_pos_shm/'
+where parameter_name = 'folder_path' and parameter_group_id = '5086';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/super_8/ph_non_pos_super_8/'
+where parameter_name = 'folder_path' and parameter_group_id = '5087';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/robinsons_ds/ph_non_pos_robinsons_ds'
+where parameter_name = 'folder_path' and parameter_group_id = '5088';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos_uat/transaction/robinsons_sm/ph_non_pos_robinsons_sm'
+where parameter_name = 'folder_path' and parameter_group_id = '5089';
