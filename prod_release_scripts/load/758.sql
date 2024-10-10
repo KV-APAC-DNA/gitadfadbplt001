554b4951574a4b90be709e82eef85d905650ad52
mmadhu1988 <mmadhu@kenvue.com>
2024-10-10 17:35:24 +0530

    Create 758.sql
    Signed-off-by: mmadhu1988 <mmadhu@kenvue.com>

---
+++ prod_release_scripts/load/758.sql
@@ -0,0 +1,21 @@
+
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/svi_smc_test/ph_non_pos_svi_smc_test'
+where parameter_name = 'folder_path' and parameter_group_id = '5080';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/waltermart_test/ph_non_pos_waltermart_test'
+where parameter_name = 'folder_path' and parameter_group_id = '5081';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/landmark_sm_test/ph_non_pos_landmark_sm_test/'
+where parameter_name = 'folder_path' and parameter_group_id = '5082';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/rustans_test/ph_non_pos_rustans_test/'
+where parameter_name = 'folder_path' and parameter_group_id = '5083';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/puregold_test/ph_non_pos_puregold_test'
+where parameter_name = 'folder_path' and parameter_group_id = '5084';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/landmark_ds_test/ph_non_pos_landmark_ds_test'
+where parameter_name = 'folder_path' and parameter_group_id = '5085';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/shm_test/ph_non_pos_shm_test/'
+where parameter_name = 'folder_path' and parameter_group_id = '5086';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/super_8_test/ph_non_pos_super_8_test/'
+where parameter_name = 'folder_path' and parameter_group_id = '5087';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/robinsons_ds_test/ph_non_pos_robinsons_ds_test'
+where parameter_name = 'folder_path' and parameter_group_id = '5088';
+update PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE = 'non_pos/transaction/robinsons_sm_test/ph_non_pos_robinsons_sm_test'
+where parameter_name = 'folder_path' and parameter_group_id = '5089';
