1f654d0fac504e60a6046841ab0f1552d0461ec2
GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>
2024-10-10 14:43:28 +0530

    Create 756.sql
    Full load OTIF
    
    Signed-off-by: GunjanSingh112 <159107949+GunjanSingh112@users.noreply.github.com>

---
+++ prod_release_scripts/load/756.sql
@@ -0,0 +1,1 @@
+UPDATE PROD_DNA_LOAD.META_RAW.PARAMETERS SET PARAMETER_VALUE='SELECT * from delv.otif_d_cnsmr_attr_detl WHERE RGN_CD = "APAC"' WHERE parameter_group_id =2001 and parameter_name = 'ms_query'
