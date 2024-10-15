20fe27c4ed7819d964047844229ba068e5fac148
mmadhu1988 <mmadhu@kenvue.com>
2024-10-15 20:15:37 +0530

    Create 765.sql
    Signed-off-by: mmadhu1988 <mmadhu@kenvue.com>

---
+++ prod_release_scripts/load/765.sql
@@ -0,0 +1,38 @@
+update prod_dna_load.meta_raw.process set process_id = 2290,  parameter_group_id = 2290,
+pipeline_type_id = 2290 where process_id = '5078';
+update prod_dna_load.meta_raw.process set process_id = 2291,  parameter_group_id = 2291,
+pipeline_type_id = 2291 where process_id = '5079';
+update prod_dna_load.meta_raw.process set process_id = 2292,  parameter_group_id = 2292,
+pipeline_type_id = 2292 where process_id = '5080';
+update prod_dna_load.meta_raw.process set process_id = 2293,  parameter_group_id = 2293,
+pipeline_type_id = 2293 where process_id = '5081';
+update prod_dna_load.meta_raw.process set process_id = 2294,  parameter_group_id = 2294,
+pipeline_type_id = 2294 where process_id = '5082';
+update prod_dna_load.meta_raw.process set process_id = 2295,  parameter_group_id = 2295,
+pipeline_type_id = 2295 where process_id = '5083';
+update prod_dna_load.meta_raw.process set process_id = 2296,  parameter_group_id = 2296,
+pipeline_type_id = 2296 where process_id = '5084';
+update prod_dna_load.meta_raw.process set process_id = 2297,  parameter_group_id = 2297,
+pipeline_type_id = 2297 where process_id = '5085';
+update prod_dna_load.meta_raw.process set process_id = 2298,  parameter_group_id = 2298,
+pipeline_type_id = 2298 where process_id = '5086';
+update prod_dna_load.meta_raw.process set process_id = 2299,  parameter_group_id = 2299,
+pipeline_type_id = 2299 where process_id = '5087';
+update prod_dna_load.meta_raw.process set process_id = 2300,  parameter_group_id = 2300,
+pipeline_type_id = 2300 where process_id = '5088';
+update prod_dna_load.meta_raw.process set process_id = 2301,  parameter_group_id = 2301,
+pipeline_type_id = 2301 where process_id = '5089';
+
+
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2290 where parameter_group_id = '5078';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2291 where parameter_group_id = '5079';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2292 where parameter_group_id = '5080';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2293 where parameter_group_id = '5081';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2294 where parameter_group_id = '5082';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2295 where parameter_group_id = '5083';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2296 where parameter_group_id = '5084';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2297 where parameter_group_id = '5085';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2298 where parameter_group_id = '5086';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2299 where parameter_group_id = '5087';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2300 where parameter_group_id = '5088';
+update prod_dna_load.meta_raw.parameters set parameter_group_id = 2301 where parameter_group_id = '5089';
