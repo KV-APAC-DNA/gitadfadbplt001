0e9ca0a6b9d131cbfa76bfa68e32f69a45e4f757
mmadhu1988 <mmadhu@kenvue.com>
2024-10-16 16:57:32 +0530

    Create 771.sql
    Signed-off-by: mmadhu1988 <mmadhu@kenvue.com>

---
+++ prod_release_scripts/load/771.sql
@@ -0,0 +1,2 @@
+update PROD_DNA_LOAD.META_RAW.PARAMETERS set parameter_value = PHLSDL_RAW.PH_NON_POS_SVI_SMC_PREPROCESSING_TEST 
+where parameter_id = 27993 and parameter_group_id = 2292 and parameter_name = 'sp_name';
