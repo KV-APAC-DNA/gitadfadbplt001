9029addd6fce7bc1d53f02e4b923f73481ee3512
MKhadk02 <149656461+MKhadk02@users.noreply.github.com>
2024-10-18 12:34:50 +0530

    Create 535.sql
    Signed-off-by: MKhadk02 <149656461+MKhadk02@users.noreply.github.com>

---
+++ prod_release_scripts/core/535.sql
@@ -0,0 +1,2 @@
+delete from PROD_DNA_CORE.INDITG_INTEGRATION.itg_ecommerce_offtake_amazon
+where source_file_name in ('Offtake_India_Amazon_Sep24','Offtake_India_Amazon_Aug24','Offtake_India_Amazon_Jul24');
