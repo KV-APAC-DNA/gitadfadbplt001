d95680a83b23cf48f254353a2c38b4b8479191b4
VasundharaU <vujawane@kenvue.com>
2024-10-23 17:39:33 +0530

    Create 581.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/581.sql
@@ -0,0 +1,7 @@
+create table aspedw_integration.edw_rpt_sfa_pm_bkp_20241023 as select * from aspedw_integration.edw_rpt_sfa_pm;
+
+create table aspitg_integration.itg_photo_mgmnt_url_bkp_20241023 as select * from aspitg_integration.itg_photo_mgmnt_url;
+
+create table aspedw_integration.edw_pop6_pm_bkp_20241023 as select * from aspedw_integration.edw_pop6_pm ;
+
+create table aspwks_integration.wks_PHOTO_MGMNT_URL_CNT_bkp_20241023 as select * from aspwks_integration.wks_PHOTO_MGMNT_URL_CNT;
