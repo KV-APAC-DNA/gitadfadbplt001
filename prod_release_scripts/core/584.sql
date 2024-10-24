3cb873ce2e0a2dd2eed077f30da277e38ca26e55
dkanun02 <165705679+dkanun02@users.noreply.github.com>
2024-10-24 14:07:58 +0530

    Create 584.sql
    Signed-off-by: dkanun02 <165705679+dkanun02@users.noreply.github.com>

---
+++ prod_release_scripts/core/584.sql
@@ -0,0 +1,4 @@
+delete from PROD_DNA_CORE.PCFEDW_INTEGRATION.EDW_DEMAND_FORECAST_SNAPSHOT_temp
+where pac_subsource_type='SAPBW_ACTUAL'
+and jj_year in (2016,2017,2018)
+and to_char(snap_shot_dt, 'YYYYMM') in (202404,202405,202406,202407,202408,202409);
