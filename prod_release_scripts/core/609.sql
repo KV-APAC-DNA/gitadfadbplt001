d28b20dd19b0c88f927d09ccfa4e70718a94d56b
AMAKHLOG <amakhlog@kenvue.com>
2024-10-29 14:15:33 +0530

    Update and rename 606.sql to 609.sql
    Signed-off-by: AMAKHLOG <amakhlog@kenvue.com>

--- prod_release_scripts/core/609.sql
+++ prod_release_scripts/core/609.sql
@@ -1,11 +1,10 @@
-
-delete from PROD_DNA_CORE.ASPEDW_INTEGRATION.EDW_RPT_REGIONAL_SELLOUT_OFFTAKE WHERE country_code='VN' AND mnth_id>= (case when (select param_value from PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG where code='base_load_vn')='ALL' THEN '190001' ELSE to_char(add_months(to_date(convert_timezone('UTC',current_timestamp())), -((select param_value from PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG where code='base_load_vn')::integer)), 'YYYYMM') END);
+delete from PROD_DNA_CORE.ASPEDW_INTEGRATION.EDW_RPT_REGIONAL_SELLOUT_OFFTAKE WHERE country_code='TW' AND mnth_id>= (case when (select param_value from PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG where code='base_load_tw')='ALL' THEN '190001' ELSE to_char(add_months(to_date(convert_timezone('UTC',current_timestamp())), -((select param_value from PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG where code='base_load_tw')::integer)), 'YYYYMM') END);
 
 insert into PROD_DNA_CORE.ASPEDW_INTEGRATION.EDW_RPT_REGIONAL_SELLOUT_OFFTAKE (
 
-with wks_vietnam_regional_sellout_offtake_npd as 
+with wks_taiwan_regional_sellout_offtake_npd as 
 (
-	 select * from PROD_DNA_CORE.VNMWKS_INTEGRATION.WKS_VIETNAM_REGIONAL_SELLOUT_OFFTAKE_NPD
+    select * from PROD_DNA_CORE.NTAWKS_INTEGRATION.WKS_TAIWAN_REGIONAL_SELLOUT_OFFTAKE_NPD
 ),
 sdl_raw_sap_bw_price_list as
 (
@@ -21,8 +20,8 @@ itg_mds_ap_customer360_config as (
 edw_list_price as
 (
     select * from PROD_DNA_CORE.aspedw_integration.edw_list_price
-) 
- 
+)
+
  SELECT * FROM (SELECT main.year::integer as year,
         main.qrtr_no,
         main.mnth_id,
@@ -89,9 +88,9 @@ edw_list_price as
         CASE WHEN FIRST_SCAN_FLAG_MARKET_LEVEL_INITIAL='Y' AND cnt_mkt>0 AND cal_date<=dateadd(week,(select param_value from itg_mds_ap_customer360_config where code='npd_flag_period_weeks')::integer,Market_Product_Min_Date) THEN 'Y' ELSE 'N' END AS NPD_FLAG_MARKET_LEVEL,
         CASE WHEN FIRST_SCAN_FLAG_PARENT_CUSTOMER_LEVEL_INITIAL='Y' AND cal_date<=dateadd(week,(select param_value from itg_mds_ap_customer360_config where code='npd_flag_period_weeks')::integer,Customer_Product_Min_Date) THEN 'Y' ELSE 'N' END AS NPD_FLAG_PARENT_CUSTOMER_LEVEL ,
         main.Customer_Product_Desc,
-        ltrim(main.msl_product_code,'0') as msl_product_code,
+        main.msl_product_code,
         main.msl_product_desc,
-        'NA' AS store_grade,
+        'NA' as store_grade,
         main.retail_env,
         main.channel,
         main.crtd_dttm::timestamp_ntz(9) as crtd_dttm,
@@ -99,18 +98,18 @@ edw_list_price as
         0 AS numeric_distribution,
         0 AS weighted_distribution,
         0 AS store_count_where_scanned
-        FROM WKS_VIETNAM_REGIONAL_SELLOUT_OFFTAKE_NPD main
+        FROM WKS_TAIWAN_REGIONAL_SELLOUT_OFFTAKE_NPD main
         left join (Select * from (select distinct a.material,a.dt_from,a.valid_to,a.amount,a.sls_org,b.ctry_key,row_number() OVER(PARTITION BY 
         ltrim(a.material, 0) ORDER BY to_date(a.valid_to, 'YYYYMMDD') DESC, to_date(a.dt_from, 'YYYYMMDD') DESC) AS rn FROM 
         (select material,dt_from,valid_to,max(amount) as amount,sls_org,cdl_dttm,currency from sdl_raw_sap_bw_price_list 
-        where sls_org IN (select distinct sls_org from edw_sales_org_dim where stats_crncy='VND')
+        where sls_org IN (select distinct sls_org from edw_sales_org_dim where stats_crncy='TWD')
         group by material,dt_from,valid_to,sls_org,cdl_dttm,currency) a LEFT JOIN (select distinct ctry_key,sls_org,crncy_key from edw_sales_org_dim where ctry_key<>'') b on a.sls_org=b.sls_org and a.currency=b.crncy_key and a.cdl_dttm = (select max(cdl_dttm) 
         from sdl_raw_sap_bw_price_list)) where rn=1) lp1 on ltrim(main.sku_code,'0') = ltrim(lp1.material, '0') and 
         TRIM(UPPER(main.country_code))=TRIM(UPPER(lp1.ctry_key)) and (main.cal_date between to_date(dt_from, 'YYYYMMDD') and 
         to_date(valid_to, 'YYYYMMDD'))   
         left join (Select * from (SELECT distinct ltrim(material, 0) AS material, amount, row_number() OVER(PARTITION BY 
         ltrim(material, 0) ORDER BY to_date(valid_to, 'YYYYMMDD') DESC, to_date(dt_from, 'YYYYMMDD') DESC) AS rn FROM edw_list_price 
-        WHERE sls_org in (select distinct sls_org from edw_sales_org_dim where ctry_key = 'VN')) where rn = 1) lp2 on ltrim(main.sku_code, '0') = ltrim(lp2.material, '0')
+        WHERE sls_org in (select distinct sls_org from edw_sales_org_dim where ctry_key = 'TW')) where rn = 1) lp2 on ltrim(main.sku_code, '0') = ltrim(lp2.material, '0')
         )              
-        where year > (select max(year) from WKS_VIETNAM_REGIONAL_SELLOUT_OFFTAKE_NPD)::integer - (select param_value from itg_mds_ap_customer360_config where code='retention_years')::integer
+        where year > (select max(year) from WKS_TAIWAN_REGIONAL_SELLOUT_OFFTAKE_NPD)::integer - (select param_value from itg_mds_ap_customer360_config where code='retention_years')::integer
 );
