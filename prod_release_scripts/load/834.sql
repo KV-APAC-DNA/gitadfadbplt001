9c1bf067cff88aaf55a5d62089d9fb9f588bec4c
AMAKHLOG <amakhlog@kenvue.com>
2024-11-08 11:20:38 +0530

    Update and rename 833.sql to 834.sql
    Signed-off-by: AMAKHLOG <amakhlog@kenvue.com>

--- prod_release_scripts/load/834.sql
+++ prod_release_scripts/load/834.sql
@@ -1,2 +1,2 @@
 update prod_dna_load.meta_raw.parameters set parameter_value='POS,projectName:eq:North+Asia' 
-where PARAMETER_GROUP_ID = 1972 AND PARAMETER_VALUE='tableauSourceName';
+where PARAMETER_GROUP_ID = 1972 AND PARAMETER_NAME='tableauSourceName';
