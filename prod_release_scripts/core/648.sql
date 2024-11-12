d35008da92e80fab007a851b8f8aa75af1f2c82a
AMAKHLOG <amakhlog@kenvue.com>
2024-11-12 21:22:42 +0530

    Update 648.sql
    Signed-off-by: AMAKHLOG <amakhlog@kenvue.com>

--- prod_release_scripts/core/648.sql
+++ prod_release_scripts/core/648.sql
@@ -1,3 +1,3 @@
 UPDATE PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG
-   SET param_value = '45'
-Where code = 'base_load_ph';
+   SET param_value = '3'
+Where code in ('base_load_ph', 'base_load_cn_otc');
