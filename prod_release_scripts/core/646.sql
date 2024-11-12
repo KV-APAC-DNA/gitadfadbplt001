966164d447cfc079853d7030110664834ae13b97
sa9-kenvue-com <160713610+sa9-kenvue-com@users.noreply.github.com>
2024-11-12 17:55:06 +0530

    Create 646.sql
    Signed-off-by: sa9-kenvue-com <160713610+sa9-kenvue-com@users.noreply.github.com>

---
+++ prod_release_scripts/core/646.sql
@@ -0,0 +1,16 @@
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_CHANNEL_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_CHANNEL;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_CHANNEL_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_CHANNEL;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_PRODUCT_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_PRODUCT;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_SUMMARY_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_SUMMARY;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_TARGET_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PLAN_TARGET;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PRODUCT_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_MC_CYCLE_PRODUCT;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_SAMPLE_LOT_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_SAMPLE_LOT;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_SAMPLE_RECEIPT_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_SAMPLE_RECEIPT;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_SURVEY_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_SURVEY;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_SURVEY_QUESTION_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_SURVEY_QUESTION;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_SURVEY_TARGET_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_SURVEY_TARGET;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_GROUP_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_GROUP;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_GROUP_MEMBER_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_GROUP_MEMBER;
+create table PROD_DNA_CORE.hcposeitg_integration.ITG_SAMPLE_TRANSACTION_12112024 as select * from PROD_DNA_CORE.hcposeitg_integration.ITG_SAMPLE_TRANSACTION;
