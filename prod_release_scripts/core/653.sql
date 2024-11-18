c7448dcb8dae92430ec6460beaed1423a03c4543
mmadhu1988 <mmadhu@kenvue.com>
2024-11-18 17:56:38 +0530

    Create 653.sql
    Signed-off-by: mmadhu1988 <mmadhu@kenvue.com>

---
+++ prod_release_scripts/core/653.sql
@@ -0,0 +1,6 @@
+INSERT INTO dbt_cloud_pr_5458_1569.aspitg_integration.itg_custgp_cogs_fg_control (acct_hier_shrt_desc,co_cd,valid_from,valid_to,gl_acct_num,active) 
+VALUES
+  ('SCOGS','433A','2024001','2099012',140327,'Y'),
+  ('SCOGS','433A','2024001','2099012',140328,'Y'),
+  ('ICMC','433A','2024001','2099012',null,'Y'),
+  ;
