bffa64238a41b8d3edd9e2c06a6a15ae154029dd
VasundharaU <vujawane@kenvue.com>
2024-11-20 11:29:24 +0530

    Create 662.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/662.sql
@@ -0,0 +1,7 @@
+delete from IDNITG_INTEGRATION.itg_id_ps_brand_blocking where input_date like '2023%';
+delete from IDNITG_INTEGRATION.itg_id_ps_pricing where input_date like '2023%';
+delete from IDNITG_INTEGRATION.itg_id_ps_product_availability where input_date like '2023%';
+delete from IDNITG_INTEGRATION.itg_id_ps_promotion where input_date like '2023%';
+delete from IDNITG_INTEGRATION.itg_id_ps_visibility where input_date like '2023%';
+delete from IDNITG_INTEGRATION.itg_id_ps_planogram where input_date like '2023%';
+delete from IDNITG_INTEGRATION.itg_id_ps_secondary_display where input_date like '2023%';
