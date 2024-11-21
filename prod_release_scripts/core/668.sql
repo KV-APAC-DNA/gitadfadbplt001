d24cfac3c0aac3a7f19532bb0375deb838cbb21f
VasundharaU <vujawane@kenvue.com>
2024-11-21 13:55:32 +0530

    Create 668.sql
    Signed-off-by: VasundharaU <vujawane@kenvue.com>

---
+++ prod_release_scripts/core/668.sql
@@ -0,0 +1,8 @@
+delete from IDNITG_INTEGRATION.itg_id_ps_brand_blocking where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_pricing where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_product_availability where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_promotion where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_visibility where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_planogram where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_secondary_display where input_date between '2024-01-01' and '2024-09-30';
+delete from IDNITG_INTEGRATION.itg_id_ps_promotion_competitor where input_date between '2024-01-01' and '2024-09-30';
