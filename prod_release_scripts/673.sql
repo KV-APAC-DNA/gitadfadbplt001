767fa63286dbd58b81be83a5d326db5be582493c
sa9-kenvue-com <160713610+sa9-kenvue-com@users.noreply.github.com>
2024-11-21 17:39:49 +0530

    Delete prod_release_scripts/673.sql
    Signed-off-by: sa9-kenvue-com <160713610+sa9-kenvue-com@users.noreply.github.com>

--- prod_release_scripts/673.sql
+++
@@ -1,3 +0,0 @@
-create table prod_DNA_CORE.ntaitg_integration.itg_pos_21112024_cosmed as select * from prod_DNA_CORE.ntaitg_integration.itg_pos;
-delete from prod_DNA_CORE.ntaitg_integration.itg_pos where SRC_SYS_CD like 'Cosmed%' and ctry_cd='TW' and pos_dt in ('2024-09-16','2024-09-23','2024-09-30','2024-10-07');
-insert into prod_DNA_CORE.ntaitg_integration.itg_pos select distinct * from prod_DNA_CORE.ntaitg_integration.itg_pos_21112024_cosmed where SRC_SYS_CD like 'Cosmed%' and ctry_cd='TW' and pos_dt in ('2024-09-16','2024-09-23','2024-09-30','2024-10-07');
