af0d4460dc381caecf5a55e9157d4ec61bfd8162
AMAKHLOG <amakhlog@kenvue.com>
2024-11-21 15:23:49 +0530

    Update 885.sql
    Signed-off-by: AMAKHLOG <amakhlog@kenvue.com>

--- prod_release_scripts/load/885.sql
+++ prod_release_scripts/load/885.sql
@@ -1,2 +1,2 @@
-update prod_dna_load.meta_raw.proces set is_active = 'FALSE' where parameter_group_id = 1983;
+update prod_dna_load.meta_raw.process set is_active = 'FALSE' where parameter_group_id = 1983;
 update prod_dna_load.meta_raw.parameters set is_active = 'FALSE' where parameter_group_id = 1983;
