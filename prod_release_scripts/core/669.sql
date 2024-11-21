63d5bd0923ec9d63c35181ca90fce19e451f778d
AMAKHLOG <amakhlog@kenvue.com>
2024-11-21 16:56:35 +0530

    Update and rename 669.sql to 670.sql
    Signed-off-by: AMAKHLOG <amakhlog@kenvue.com>

--- prod_release_scripts/core/669.sql
+++
@@ -1,7 +0,0 @@
-UPDATE PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG
-   SET param_value = '36'
-Where code = 'base_load_cn_otc';
-
-UPDATE PROD_DNA_CORE.ASPITG_INTEGRATION.ITG_MDS_AP_CUSTOMER360_CONFIG
-   SET param_value = '52'
-Where code = 'base_load_ph';
