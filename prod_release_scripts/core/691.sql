2eb7571fcb5a72fe61971aefcdffd29b522143e5
echithat <echithat@kenvue.com>
2024-11-28 13:42:23 +0530

    Create 691.sql
    Signed-off-by: echithat <echithat@kenvue.com>

---
+++ prod_release_scripts/core/691.sql
@@ -0,0 +1,2 @@
+delete from  PROD_DNA_CORE.JPNEDW_integration.DW_POS_DAILY where account_key='DNKI' and source_file_date='20241019';
+delete from  PROD_DNA_CORE.JPNEDW_integration.DW_POS_DAILY where account_key='DNKI' and source_file_date='20241026';
