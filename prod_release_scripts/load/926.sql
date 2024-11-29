b7d8fa7c6f2cd43303c52137ffa8daccccc6c95d
sbasu06 <sbasu06@kenvue.com>
2024-11-29 14:46:05 +0530

    Add files via upload
    Signed-off-by: sbasu06 <sbasu06@kenvue.com>

---
+++ prod_release_scripts/load/926.sql
@@ -0,0 +1,3 @@
+update meta_raw.incremental_watermark set watermark_value = '2024-01-01 00:00:00' where process_id = 1150 and watermark_id = 6874;
+
+update meta_raw.incremental_watermark set watermark_value = '2024-01-01 00:00:00' where process_id = 1195 and watermark_id = 6873;
