4d013201715ed415c0115917df20d73a1ddb5d77
jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>
2024-12-02 18:56:17 +0530

    Create 709.sql
    creating clones of itg_pos, edw_pos_fact
    
    Signed-off-by: jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>

---
+++ prod_release_scripts/core/709.sql
@@ -0,0 +1,7 @@
+create or replace table 
+prod_dna_core.dbt_cloud_pr_5458_1580.ntaitg_integration__itg_pos 
+clone prod_dna_core.ntaitg_integration.itg_pos;
+
+create or replace table 
+prod_dna_core.dbt_cloud_pr_5458_1580.ntaedw_integration__edw_pos_fact 
+clone prod_dna_core.ntaedw_integration.edw_pos_fact;
