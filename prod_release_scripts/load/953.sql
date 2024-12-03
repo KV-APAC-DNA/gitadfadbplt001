0008ca8356d92edf1e738a7a6abb96010626295b
Sbhat33 <sbhat33@kenvue.com>
2024-12-03 20:10:17 +0530

    Create 953.sql
    Signed-off-by: Sbhat33 <sbhat33@kenvue.com>

---
+++ prod_release_scripts/load/953.sql
@@ -0,0 +1,4 @@
+UPDATE META_RAW.process SET process_name='IN_HCP360_MDS_Data_Refresh_SDL_MDS_IN_HCP_SpecialtyCare'
+WHERE process_id='3041';
+UPDATE META_RAW.process SET process_name='IN_HCP360_MDS_Data_Refresh_SDL_MDS_IN_HCP_SELFCARE'
+WHERE process_id='3042';
