e03b4aacd09450872e7346047a9fdd7e89eb7fd4
GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>
2024-12-09 12:00:51 +0530

    Create 972.sql
    Correct the PH POS MDS file folder path
    
    Signed-off-by: GT-pbalakri <165148169+GT-pbalakri@users.noreply.github.com>

---
+++ prod_release_scripts/load/972.sql
@@ -0,0 +1,4 @@
+update META_RAW.PARAMETERS 
+set parameter_value = (parameter_value, length(parameter_value)-1)
+where parameter_group_id in (2242, 2243, 2244, 2245, 2246, 2248, 2249, 2250, 2251, 2252, 2253)
+and PARAMETER_NAME = 'folder_path'
