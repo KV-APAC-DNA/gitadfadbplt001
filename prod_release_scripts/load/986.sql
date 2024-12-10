2bebb6a2181642a1976b7286c223335c38f037fc
sbasu06 <sbasu06@kenvue.com>
2024-12-10 14:26:25 +0530

    Add files via upload
    Signed-off-by: sbasu06 <sbasu06@kenvue.com>

---
+++ prod_release_scripts/load/986.sql
@@ -0,0 +1,19 @@
+delete from meta_raw.incremental_watermark where process_id = 1147 and watermark_id = 7693;
+
+delete from meta_raw.incremental_watermark where process_id = 1162 and watermark_id = 7714;
+
+delete from meta_raw.incremental_watermark where process_id = 1149 and watermark_id = 7695;
+
+delete from meta_raw.incremental_watermark where process_id = 1195 and watermark_id = 7696;
+
+delete from meta_raw.incremental_watermark where process_id = 1150 and watermark_id = 7692;
+
+delete from meta_raw.incremental_watermark where process_id = 1196 and watermark_id = 7718;
+
+delete from meta_raw.incremental_watermark where process_id = 1161 and watermark_id = 7698;
+
+delete from meta_raw.incremental_watermark where process_id = 1172 and watermark_id = 7679;
+
+delete from meta_raw.incremental_watermark where process_id = 1157 and watermark_id = 7708;
+
+commit;
