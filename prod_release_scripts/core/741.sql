3c8249385de37c7ebe6179cf2155c6bd54381c84
sbasu06 <sbasu06@kenvue.com>
2024-12-11 16:36:48 +0530

    Add files via upload
    Signed-off-by: sbasu06 <sbasu06@kenvue.com>

---
+++ prod_release_scripts/core/741.sql
@@ -0,0 +1,14 @@
+truncate table PROD_DNA_CORE.JPDCLITG_INTEGRATION.contact_hist;
+
+insert into PROD_DNA_CORE.JPDCLITG_INTEGRATION.contact_hist
+with source as
+(
+    select * from PROD_DNA_LOAD.jpdclsdl_raw.contact_hist
+),
+final as
+(
+    select * from source
+)
+select * from final;
+
+commit;
