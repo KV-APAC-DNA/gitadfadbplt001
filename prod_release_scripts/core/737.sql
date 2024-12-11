669bb717bba36d09c4d0c53a58edff8292588b64
kkuma011 <KKuma011@kenvue.com>
2024-12-11 14:25:03 +0530

    Added 737.sql
    Signed-off-by: kkuma011 <KKuma011@kenvue.com>

---
+++ prod_release_scripts/core/737.sql
@@ -0,0 +1,12 @@
+create or replace table prod_dna_core.inditg_integration.itg_query_parameters_bkp_121124 as (select * from prod_dna_core.inditg_integration.itg_query_parameters);
+
+insert into prod_dna_core.inditg_integration.itg_query_parameters values
+('IN','ORSL','self_care','Add_prof_heir'),
+('IN','Benadryl','self_care','Add_prof_heir'),
+('IN','Coldarin','self_care','Add_prof_heir'),
+('IN','DAKTARIN','self_care','Add_prof_heir'),
+('IN','IMODIUM','self_care','Add_prof_heir'),
+('IN','Aveeno Baby','speciality','Add_prof_heir'),
+('IN','AVEENO','speciality','Add_prof_heir'),
+('IN','Regaine','speciality','Add_prof_heir'),
+('IN','Neutrogena','speciality','Add_prof_heir');
