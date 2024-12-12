4cfb2bcbbbb21244b95919537d6c8d5cc1719afb
sbasu06 <sbasu06@kenvue.com>
2024-12-12 17:05:15 +0530

    Add files via upload
    Signed-off-by: sbasu06 <sbasu06@kenvue.com>

---
+++ prod_release_scripts/core/753.sql
@@ -0,0 +1,26 @@
+INSERT  OVERWRITE INTO JPDCLEDW_INTEGRATION.dm_user_attr_sfcc_v_prev
+select  * from JPDCLEDW_INTEGRATION.dm_user_attr_sfcc_v_prev1;
+
+commit;
+
+INSERT  OVERWRITE INTO JPDCLEDW_INTEGRATION.dm_kesai_mart_dly_general_prev
+select  * from JPDCLEDW_INTEGRATION.dm_kesai_mart_dly_general_prev1;
+
+commit;
+
+INSERT  OVERWRITE INTO JPDCLEDW_INTEGRATION.dm_user_attr_prev
+select  * from JPDCLEDW_INTEGRATION.dm_user_attr_prev1;
+
+commit;
+
+INSERT  OVERWRITE INTO JPDCLEDW_INTEGRATION.kr_new_stage_point_prev
+select  * from JPDCLEDW_INTEGRATION.kr_new_stage_point_prev1;
+
+commit;
+
+
+INSERT  OVERWRITE INTO JPDCLEDW_INTEGRATION.teikikeiyaku_data_mart_uni_prev
+select  * from JPDCLEDW_INTEGRATION.teikikeiyaku_data_mart_uni_prev1;
+
+commit;
+
