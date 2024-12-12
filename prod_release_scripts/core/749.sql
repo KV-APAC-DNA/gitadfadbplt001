a1490ab16a217c3fb2372ccbf1bc17744b21e4d5
jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>
2024-12-12 15:58:21 +0530

    Create 749.sql
    Signed-off-by: jc3-jeeva <148064226+jc3-jeeva@users.noreply.github.com>

---
+++ prod_release_scripts/core/749.sql
@@ -0,0 +1,2 @@
+DELETE FROM PROD_DNA_CORE.DBT_CLOUD_PR_5458_1580.ntaedw_integration__edw_pos_fact where src_sys_cd like 'A-M%';
+DELETE FROM PROD_DNA_CORE.DBT_CLOUD_PR_5458_1580.NTAITG_INTEGRATION__ITG_POS where src_sys_cd like 'A-M%';
